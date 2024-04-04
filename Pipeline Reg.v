b51a23a13c4750171b9203e1a5092b393b1be270
ceuncha <155208795+ceuncha@users.noreply.github.com>
2024-04-04 15:14:05 +0900

    Update Pipeline Reg.v

--- Pipeline Reg.v
+++ Pipeline Reg.v
@@ -1,12 +1,5 @@
 /*
-파이프라인 내 요소는 IF_ID_ALUSrc 형태로 기술함
-Control 신호는 아직 미완이라 하나로 뭉쳐 놓음
-앞으로 할 일:
-기능 추가될 때마다 꼼꼼히 유지보수
-Control 신호를 베릴로그 헤더 파일 사용해서 협업해도 좋을듯. 예를 들면
-`define PCSrc 0
-`define Jump 1
-`define ALUOp 4:2
+ver.0404 12:52
 */
 
 //IFID PIPELINE REGISTER
@@ -36,27 +29,36 @@ module idex_pipeline_register #(
     )(    
     input clk,
     input [Control_Width-1:0] Control_Sig,
-    input [4:0] Rs1, Rs2, Rd,
+    input [4:0] IF_ID_Rs1, IF_ID_Rs2, IF_ID_Rd,
+    input [2:0] IF_ID_funct3;
     input [31:0] RData1, RData2,
     input [31:0] imm32,  // sign extend의 output
-    input [31:0] If_ID_PC,
-    input [2:0] funct3;
+    input [31:0] PC_Plus4, PC_imm, imm_out,
+
     output reg [Control_Width-1:0] ID_EX_Control_Sig,
     output reg [4:0] ID_EX_Rs1, ID_EX_Rs2, ID_EX_Rd,
+    output reg [2:0] ID_EX_funct3;
     output reg [31:0] ID_EX_RData1, ID_EX_RData2,
     output reg [31:0] ID_EX_imm32,
-    output reg [31:0] ID_EX_PC.
-    output reg [2:0] ID_EX_funct3;
+    output reg [31:0] ID_EX_PC_Plus4, ID_EX_PC_imm, ID_EX_imm_out,
 );
         
     always @(posedge clk) begin
-        ID_EX_Control_Sig <= Control_Sig;
-        ID_EX_Rs1 <= Rs1;
-        ID_EX_Rs2 <= Rs2;
-        ID_EX_Rd <= Rd;
-        ID_EX_imm32 <= imm32;
-        ID_EX_PC <= IF_ID_PC;
-        ID_EX_funct3 <= funct3;
+        if(!Control_Sig_Stall) begin
+            ID_EX_Control_Sig <= Control_Sig;
+            ID_EX_Rs1 <= IF_ID_Rs1;
+            ID_EX_Rs2 <= IF_ID_Rs2;
+            ID_EX_Rd <= IF_ID_Rd;
+            ID_EX_funct3 <= IF_ID_funct3;
+            ID_EX_imm32 <= imm32;
+            ID_EX_PC_Plus4 <= PC_Plus4;
+            ID_EX_PC_imm <= PC_imm;
+            ID_EX_imm_out <= imm_out;
+        end
+        else begin
+            // Stall 발생 시 동작
+        end
+        
     end 
 endmodule
 
@@ -67,12 +69,12 @@ module exmem_pipeline_register (
     input [4:0] ID_EX_Rd, // inst decode해서 나온 dest reg가 넘어온 것
     input [31:0] ALUResult,  // ALU output
     input [31:0] ID_EX_RData2,  // regfile의 output 중 2가 넘어온 것
-    input [31:0] PC_Plus4,  // Jump 명령어 구현을 위해 필요
+    input [31:0] ID_EX_PC_Plus4, ID_EX_PC_imm, ID_EX_imm_out, // Branch unit에서 넘어온 것
     output reg [Control_Width-1:0] EX_MEM_Control_Sig,
     output reg [4:0] EX_MEM_Rd,
     output reg [31:0] EX_MEM_ALUResult,
     output reg [31:0] EX_MEM_RData2,
-    output reg [31:0] EX_MEM_PC_Plus4
+    output reg [31:0] EX_MEM_PC_Plus4, EX_MEM_PC_imm, EX_MEM_imm_out
 );
         
     always @(posedge clk) begin
@@ -80,7 +82,9 @@ module exmem_pipeline_register (
         EX_MEM_Rd <= ID_EX_Rd;
         EX_MEM_ALUResult <= ALUResult;
         EX_MEM_RData2 <= ID_EX_RData2;
-        EX_MEM_PC_Plus4 <= PC_Plus4;
+        EX_MEM_PC_Plus4 <= ID_EX_PC_Plus4;
+        EX_MEM_PC_imm <= ID_EX_PC_imm;
+        EX_MEM_imm_out <= ID_EX_imm_out;
     end 
 endmodule
 
@@ -89,18 +93,23 @@ module memwb_pipeline_register (
     input clk;
     input [Control_Width-1:0] EX_MEM_Control_Sig,
     input [4:0] EX_MEM_Rd;  // inst decode해서 나온 dest reg가 넘어온 것
-    input [31:0] EX_MEM_PC_Plus4,
+    input [31:0] EX_MEM_PC_Plus4, EX_MEM_PC_imm, EX_MEM_imm_out
+    input [31:0] EX_MEM_ALUResult;
     input [31:0] RData; // data memory에서 읽은 데이터
     output reg [Control_Width-1:0] MEM_WB_Control_Sig,
     output reg [4:0] MEM_WB_Rd;
-    output reg [31:0] MEM_WB_PC_Plus4
+    output reg [31:0] MEM_WB_PC_Plus4, MEM_WB_PC_imm, MEM_WB_imm_out,
+    output reg [31:0] MEM_WB_ALUResult;
     output reg [31:0] MEM_WB_RData;
 );
         
     always @(posedge clk) begin
         MEM_WB_Control_Sig <= EX_MEM_Control_Sig;
         MEM_WB_Rd <= EX_MEM_Rd;
-        EX_MEM_PC_Plus4 <= MEM_WB_PC_Plus4;
+        MEM_WB_PC_Plus4 <= EX_MEM_PC_Plus4;
+        MEM_WB_PC_imm <= EX_MEM_PC_imm;
+        MEM_WB_imm_out <= EX_MEM_imm_out;
+        MEM_WB_ALUResult <= EX_MEM_ALUResult;
         MEM_WB_RData <= RData;
     end 
     
