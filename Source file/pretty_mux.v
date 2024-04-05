16f8120525efd2b6e4e8c63944985ed5a3e8e16a
BAESANGWON1 <165987407+BAESANGWON1@users.noreply.github.com>
2024-04-05 22:19:50 +0900

    Update pretty_mux.v

--- Source file/pretty_mux.v
+++ Source file/pretty_mux.v
@@ -1,39 +1,16 @@
-module pretty_mux (
-
-    input wire [31:0] mem_out,
-
-    input wire [31:0] imm_out,
-
-    input wire [31:0] PC_imm,
-
-    input wire [31:0] MEM_WB_PC_Plus4,
-
-    input wire [1:0] MEM_WB_RWSet,
-
-    output reg [31:0] Write_Data
-
+module pretty_mux(
+    input wire [31:0] mem_out,          
+    input wire [31:0] MEM_WB_Rd_data,   
+    input wire MEM_WB_RWSet,            
+    output reg [31:0] Write_Data       
 );
 
-
-
-always @(*) begin
-
-    case (MEM_WB_RWSet)
-
-        2'b00: Write_Data = mem_out; // MEM_WB_RWSet이 00이면 mem_out 출력
-
-        2'b01: Write_Data = imm_out; // MEM_WB_RWSet이 01이면 imm_out 출력
-
-        2'b10: Write_Data = PC_imm; // MEM_WB_RWSet이 10이면 PC_imm 출력
-
-        2'b11: Write_Data = MEM_WB_PC_Plus4; // MEM_WB_RWSet이 11이면 MEM_WB_PC_Plus4 출력
-
-        default: Write_Data = 32'b0; // 그 외의 경우에는 기본값으로 0을 출력
-
-    endcase
-
-end
-
-
+    always @(*) begin
+        if (MEM_WB_RWSet == 1'b0) begin
+            Write_Data = mem_out;       
+        end else if (MEM_WB_RWSet == 1'b1) begin
+            Write_Data = MEM_WB_Rd_data;
+        end
+    end
 
 endmodule
