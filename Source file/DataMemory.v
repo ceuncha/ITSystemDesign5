94f6659160abf99ff7bf6410ba483a307e5f06fa
ceuncha <155208795+ceuncha@users.noreply.github.com>
2024-04-29 13:38:11 +0900

    Fix inferred latch problem

--- Source file/DataMemory.v
+++ Source file/DataMemory.v
@@ -10,6 +10,7 @@ module DataMemory(
 reg [31:0] memory [0:255];
 
 always @ (*) begin
+    ReadData = 32'd0; // Default value assignment to handle cases when MemRead is false
     if (MemRead) begin
         case (funct3)
             3'b000: ReadData <= {{24{memory[ALUResult][31]}}, memory[ALUResult][7:0]}; // LB
@@ -17,10 +18,7 @@ always @ (*) begin
             3'b010: ReadData <= memory[ALUResult]; // LW
             3'b100: ReadData <= {{24{1'b0}}, memory[ALUResult][7:0]}; // LBU
             3'b101: ReadData <= {{16{1'b0}}, memory[ALUResult][15:0]}; // LHU
-            default: ReadData <= 32'd0;
         endcase
-    end else begin
-        ReadData = 32'd0; // Default value to avoid inferred latches
     end
 end
 
@@ -30,6 +28,7 @@ always @ (*) begin
             3'b000: memory[ALUResult][7:0] <= WriteData[7:0]; // SB
             3'b001: memory[ALUResult][15:0] <= WriteData[15:0]; // SH
             3'b010: memory[ALUResult] <= WriteData; // SW
+            default: ; // No operation (NOP) for other funct3 values, explicitly defined
         endcase
     end
 end
