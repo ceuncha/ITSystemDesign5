d7f4096e4207c7c1dc4654a213a0c561ba4bec74
jjurijang <165987742+jjurijang@users.noreply.github.com>
2024-05-26 17:23:42 +0900

    Update Program_Counter.v

--- Source file/Program_Counter.v
+++ Source file/Program_Counter.v
@@ -1,32 +1,26 @@
-module Program_Counter(clk, PC_Branch, PC_Stall,PCSrc, PC);
-    input clk;
-    input [31:0] PC_Branch;
-    input PC_Stall;
-    input PCSrc;
-    output reg [31:0] PC;
-   
-    reg [31:0] PC_Plus4;
-   
-   initial begin
-   PC= 32'd0;
-   PC_Plus4 = 32'd0;
-   end
-   
-always @ (*) begin
-    PC_Plus4 <= PC + 32'd4;
-end
+module Program_Counter(
+    input clk,
+    input rst,
+    input [31:0] PC_Branch,
+    input PCSrc,
+    output reg [31:0] PC
+);
 
-always @ (posedge clk) begin
-    if (PC_Stall) begin
-        // stall signal = 1 , keep PC unchanged
-        PC <= PC;
-    end else begin
-        if(PCSrc==1) begin
+    reg [31:0] PC_Plus4;
+
+    always @ (*) begin
+        PC_Plus4 <= PC + 32'd4;
+    end
+
+    always @ (posedge clk or posedge rst) begin
+        if (rst) begin
+            PC <= 32'd0;
+            PC_Plus4 <= 32'd0;
+        end else if (PCSrc) begin
             PC <= PC_Branch;
-        end else if (PCSrc==0) begin
+        end else begin
             PC <= PC_Plus4;
         end
     end
-end
 
 endmodule
