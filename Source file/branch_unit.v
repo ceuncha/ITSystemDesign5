9af3d06aa212bfa09005ec1781574e2f3d8e879f
jjurijang <165987742+jjurijang@users.noreply.github.com>
2024-05-26 16:23:32 +0900

    Update branch_unit.v

--- Source file/branch_unit.v
+++ Source file/branch_unit.v
@@ -5,13 +5,14 @@ module BranchUnit(
     input wire [2:0] ID_EX_funct3,
     input wire [31:0] ALUResult,
     input wire [31:0] imm,
-    input wire [9:0] PC,
+    input wire [31:0] PC,
     input wire ALUNegative, // ALU에서 오는 Negative 플래그
     input wire ALUZero,     // ALU에서 오는 Zero 플래그
     input wire ALUOverflow, // ALU에서 오는 Overflow 플래그
     input wire ALUCarry,    // ALU에서 오는 Carry 플래그
     input wire ALUOperationComplete, // ALU에서 오는 연산 완료 신호
     output reg [31:0] PC_Branch,
+    output reg [31:0] Branch_index
     output reg PCSrc,
     output reg IF_ID_Flush,
     output reg ID_RS_Flush
@@ -29,6 +30,7 @@ always @(*) begin
             PCSrc = 1; // 점프 시 PC 소스 신호 활성화
             IF_ID_Flush = 1'b1;
             ID_RS_Flush = 1'b1;
+            Branch_index = PC
         end
         else if(ID_EX_Branch) begin
             case(ID_EX_funct3)
@@ -44,6 +46,7 @@ always @(*) begin
                 PC_Branch = imm + PC + 4; // 분기 주소 업데이트
                 IF_ID_Flush = 1'b1;
                 ID_RS_Flush = 1'b1;
+                Branch_index = PC
             end
         end
         else begin
