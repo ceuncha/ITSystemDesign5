f90a4e14f550b67946994a5a6b6210b0e710cc30
minseo20000 <165993774+minseo20000@users.noreply.github.com>
2024-05-29 21:26:37 +0900

    Update A_select.v

--- Source file/A_select.v
+++ Source file/A_select.v
@@ -1,12 +1,12 @@
 module A_select(
 input [31:0] A,
 input [31:0] B,
-input ID_EX_jump,
-input ALUSrc,
+input sel,
+input sel2,
 output reg [31:0] y
 );
 always@(*) begin
-if(ID_EX_jump&&(~ALUSrc)) begin
+if(sel&&!sel2) begin
 y=A;
 end 
 else begin
