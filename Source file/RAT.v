9edbe0b801ea8f4989bea8b1f05376f80b97d4e3
hominki <165993704+hominki@users.noreply.github.com>
2024-05-31 15:53:32 +0900

    Update RAT.v
    레디 비트 제거

--- Source file/RAT.v
+++ Source file/RAT.v
@@ -1,4 +1,4 @@
-module RAT (
+mmodule RAT (
 input wire reset,
 input wire write_enable, // 쓰기 활성화 신호
 input wire [4:0] logical_addr1, // 오퍼랜드 1 논리 주소
@@ -35,7 +35,7 @@ integer i;
         
         for (i = 0; i < 32; i = i + 1) begin
             phy_addr_table[i] <= i;
-            valid_table[i] <= 1'b1;
+
         end
         
     end 
@@ -44,20 +44,18 @@ integer i;
 
         case (opcode)
         7'b1100111, 7'b0000011, 7'b0010011: begin  //jalr, load, i-type
-            ready_out[0] <= valid_table[logical_addr1];
-            ready_out[1] <= 1;
+
             phy_addr_out1 <= phy_addr_table[logical_addr1];
             phy_addr_out2 <= 0;
         end
         7'b0110111, 7'b0010111, 7'b1101111: begin // lui, auipc, jal
-            ready_out <= 2'b11;
+ 
             phy_addr_out1 <= 0;
             phy_addr_out2 <= 0;
             
         end
         default: begin
-            ready_out[1] <= valid_table[logical_addr2];
-            ready_out[0] <= valid_table[logical_addr1];
+
             phy_addr_out1 <= phy_addr_table[logical_addr1];
             phy_addr_out2 <= phy_addr_table[logical_addr2];
         end
@@ -67,7 +65,7 @@ integer i;
         
         // 2. Rd 레지스터 유효성 및 새로운 물리 주소 할당
         if((opcode != 7'b1100011) && (opcode != 7'b0100011)) begin  // beq, store need no Rd
-            valid_table[rd_logical_addr] <= 1'b0; // 유효성을 0으로 변경
+
             free_phy_addr_out <= phy_addr_table[rd_logical_addr]; //프리리스트로 비어있는 주소 전송 
             phy_addr_table[rd_logical_addr] <= free_phy_addr; // 새로운 물리 주소 할당
             rd_phy_out <= free_phy_addr;
