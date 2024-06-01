09b3331f33fa12124974c15f45e864dc1940ffce
hominki <165993704+hominki@users.noreply.github.com>
2024-06-01 22:06:47 +0900

    Update RS_EX_Decoder.v

--- Source file/RS_EX_Decoder.v
+++ Source file/RS_EX_Decoder.v
@@ -10,7 +10,10 @@ module RS_EX_decoder(
     input [31:0] in_label,  // 조건 분기 명령어에서 사용될 Immediate 값
     input [6:0] in_control, // 7-bit, 제어 신호
     input [7:0] rd_phy_reg, // 8-bit, 물리적 레지스터 주소
-
+    input [7:0] Operand1_phy,  
+    input [7:0] Operand2_phy,
+    input [1:0] valid,
+    input [31:0] immediate,
 
     output reg [31:0] add_alu_operand1,  // Add ALU로 보낼 첫 번째 피연산자
     output reg [31:0] add_alu_operand2,  // Add ALU로 보낼 두 번째 피연산자
@@ -20,7 +23,11 @@ module RS_EX_decoder(
     output reg [6:0] add_control,        // Add ALU로 보낼 제어 신호
     output reg [7:0] add_rd_phy_reg,     // Add ALU로 보낼 물리적 레지스터 주소
     output reg add_rs_on,
-
+    output reg [7:0] out_add_Operand1_phy,
+    output reg [7:0] out_add_Operand2_phy,
+    output reg [1:0] out_add_valid,
+    output reg [31:0] out_add_immediate,
+    
     output reg [31:0] mul_alu_operand1,  // Mul ALU로 보낼 첫 번째 피연산자
     output reg [31:0] mul_alu_operand2,  // Mul ALU로 보낼 두 번째 피연산자
     output reg [2:0] mul_alu_func3,      // Mul ALU로 보낼 func3
@@ -29,6 +36,10 @@ module RS_EX_decoder(
     output reg [6:0] mul_control,        // Mul ALU로 보낼 제어 신호
     output reg [7:0] mul_rd_phy_reg,     // Mul ALU로 보낼 물리적 레지스터 주소
     output reg mul_rs_on,
+    output reg [7:0] out_mul_Operand1_phy,
+    output reg [7:0] out_mul_Operand2_phy,
+    output reg [1:0] out_mul_valid,
+    output reg [31:0] out_mul_immediate,
 
     output reg [31:0] div_alu_operand1,  // Div ALU로 보낼 첫 번째 피연산자
     output reg [31:0] div_alu_operand2,  // Div ALU로 보낼 두 번째 피연산자
@@ -37,7 +48,11 @@ module RS_EX_decoder(
     output reg [31:0] div_alu_label,     // Div ALU로 보낼 Immediate 값
     output reg [6:0] div_control,        // Div ALU로 보낼 제어 신호
     output reg [7:0] div_rd_phy_reg,      // Div ALU로 보낼 물리적 레지스터 주소
-    output reg div_rs_on
+    output reg div_rs_on,
+    output reg [7:0] out_div_Operand1_phy,
+    output reg [7:0] out_div_Operand2_phy,
+    output reg [1:0] out_div_valid,
+    output reg [31:0] out_div_immediate
 );
 
 always @(posedge reset) begin
@@ -50,6 +65,9 @@ always @(posedge reset) begin
         add_alu_label <= 0;
         add_control <= 0;
         add_rd_phy_reg <= 0;
+        out_add_Operand1_phy <= 0;
+        out_add_Operand2_phy <= 0;
+        out_add_valid <= 0;
 
         mul_alu_operand1 <= 0;
         mul_alu_operand2 <= 0;
@@ -58,6 +76,9 @@ always @(posedge reset) begin
         mul_alu_label <= 0;
         mul_control <= 0;
         mul_rd_phy_reg <= 0;
+        out_mul_Operand1_phy <= 0;
+        out_mul_Operand2_phy <= 0;
+        out_mul_valid <= 0;
 
         div_alu_operand1 <= 0;
         div_alu_operand2 <= 0;
@@ -66,6 +87,12 @@ always @(posedge reset) begin
         div_alu_label <= 0;
         div_control <= 0;
         div_rd_phy_reg <= 0;
+        out_div_Operand1_phy <= 0;
+        out_div_Operand2_phy <= 0;
+        out_div_valid <= 0;
+        add_rs_on <= 0;
+        mul_rs_on <= 0;
+        div_rs_on <= 0;
         end
 end
 
@@ -83,6 +110,10 @@ always @(*) begin
                 add_rs_on <= 0;
                 mul_rs_on <= 1;
                 div_rs_on <= 0;
+                out_mul_Operand1_phy <= Operand1_phy;  
+                out_mul_Operand2_phy  <= Operand2_phy;
+                out_mul_valid <= valid;
+                out_mul_immediate <= immediate;
             end
             7'b1001111: begin // DIV 명령어
                 div_alu_operand1 <= in_operand1;
@@ -95,6 +126,10 @@ always @(*) begin
                 add_rs_on <= 0;
                 mul_rs_on <= 0;
                 div_rs_on <= 1;
+                out_div_Operand1_phy <= Operand1_phy;  
+                out_div_Operand2_phy <= Operand2_phy;
+                out_div_valid <= valid;
+                out_div_immediate <= immediate;
             end
             default: begin // ADD 명령어
                 add_alu_operand1 <= in_operand1;
@@ -107,6 +142,10 @@ always @(*) begin
                 add_rs_on <= 1;
                 mul_rs_on <= 0;
                 div_rs_on <= 0;
+                out_add_Operand1_phy <= Operand1_phy;
+                out_add_Operand2_phy <= Operand2_phy;
+                out_add_valid <= valid;
+                out_add_immediate <= immediate;
             end
         endcase
     end
