db02f4c897139a24b8a24527650c96ec81107a98
BAESANGWON1 <165987407+BAESANGWON1@users.noreply.github.com>
2024-06-01 18:14:53 +0900

    Update DataMemory.v

--- Source file/DataMemory.v
+++ Source file/DataMemory.v
@@ -1,39 +1,39 @@
 module DataMemory(
-    input wire MemRead,
-    input wire MemWrite,
-    input wire [2:0] funct3,
-    input wire [31:0] ALUResult,
-    input wire [31:0] WriteData,
-    output reg [31:0] ReadData
+    input wire EX_MEM_MemRead,
+    input wire EX_MEM_MemWrite,
+    input wire [2:0] EX_MEM_funct3,
+    input wire [31:0] EX_MEM_ALUResult,
+    input wire [31:0] EX_MEM_Rdata2,
+    output reg [31:0] RData
 );
 
 reg [31:0] memory [0:255];
 
 always @ (*) begin
-// Default value for ReadData, ensures it is always assigned
-    ReadData <= 32'd0; // if MemRead is false
-    if (MemRead) begin
-        case (funct3)
-            3'b000: ReadData <= {{24{memory[ALUResult][31]}}, memory[ALUResult][7:0]}; // LB
-            3'b001: ReadData <= {{16{memory[ALUResult][31]}}, memory[ALUResult][15:0]}; // LH
-            3'b010: ReadData <= memory[ALUResult]; // LW
-            3'b100: ReadData <= {{24{1'b0}}, memory[ALUResult][7:0]}; // LBU
-            3'b101: ReadData <= {{16{1'b0}}, memory[ALUResult][15:0]}; // LHU
-            default: ReadData <= 32'd0; // Default value assignment to handle cases when MemRead is false
+// Default value for RData, ensures it is always assigned
+    RData <= 32'd0; // if MemRead is false
+    if (EX_MEM_MemRead) begin
+        case (EX_MEM_funct3)
+            3'b000: RData <= {{24{memory[EX_MEM_ALUResult][31]}}, memory[EX_MEM_ALUResult][7:0]}; // LB
+            3'b001: RData <= {{16{memory[EX_MEM_ALUResult][31]}}, memory[EX_MEM_ALUResult][15:0]}; // LH
+            3'b010: RData <= memory[EX_MEM_ALUResult]; // LW
+            3'b100: RData <= {{24{1'b0}}, memory[EX_MEM_ALUResult][7:0]}; // LBU
+            3'b101: RData <= {{16{1'b0}}, memory[EX_MEM_ALUResult][15:0]}; // LHU
+            default: RData <= 32'd0; // Default value assignment to handle cases when MemRead is false
         endcase
     end
 end
 
 always @ (*) begin
-    if (MemWrite) begin
-        case (funct3)
-            3'b000: memory[ALUResult][7:0] <= WriteData[7:0]; // SB
-            3'b001: memory[ALUResult][15:0] <= WriteData[15:0]; // SH
-            3'b010: memory[ALUResult] <= WriteData; // SW
+    if (EX_MEM_MemWrite) begin
+        case (EX_MEM_funct3)
+            3'b000: memory[EX_MEM_ALUResult][7:0] <= EX_MEM_Rdata2[7:0]; // SB
+            3'b001: memory[EX_MEM_ALUResult][15:0] <= EX_MEM_Rdata2[15:0]; // SH
+            3'b010: memory[EX_MEM_ALUResult] <= EX_MEM_Rdata2; // SW
             default: ; // No operation (NOP) for other funct3 values, explicitly defined
         endcase
     end
-    else if (!MemWrite) ; // If MemWrite is false, no changes are made to memory - it retains its previous state
+    else if (!EX_MEM_MemWrite) ; // If EX_MEM_MemWrite is false, no changes are made to memory - it retains its previous state
 end
 
 endmodule
