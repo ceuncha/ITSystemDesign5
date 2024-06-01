f2127de5393eb5e325f6e31fa40bd3f8f5409881
BAESANGWON1 <165987407+BAESANGWON1@users.noreply.github.com>
2024-06-01 18:14:31 +0900

    Update Pipeline_Reg.v

--- Source file/Pipeline_Reg.v
+++ Source file/Pipeline_Reg.v
@@ -136,53 +136,71 @@ module exmem_pipeline_register (
     input ID_EX_MemToReg,
     input ID_EX_MemRead,
     input ID_EX_MemWrite,
-    input [2:0] ID_EX_funct3,
-    input [4:0] ID_EX_Rd, // inst decode
-    input [31:0] ALUResult,  // ALU output
-    input [31:0] ID_EX_RData2,  // regfile
-    input [31:0] Rd_data,
+    input [3:0] RS_EX_funt3,
+    input [31:0] operand2_Phy_Data,
+    input [31:0] RS_EX_ALUResult,
+    input [31:0] RS_EX_PC_ALU,
+    input ALU_done,
+    input [7:0] RS_EX_alu_Physical_address,
+    input [31:0] Mul_Result,
+    input [31:0] RS_EX_PC_Mul_out,
+    input Mul_done_out,
+    input [31:0] Div_Result,
+    input [31:0] RS_EX_PC_Div_out,
+    input [31:0] Div_done_out,
 
-    
     output reg EX_MEM_MemToReg,
     output reg EX_MEM_MemRead,
     output reg EX_MEM_MemWrite,
     output reg [2:0] EX_MEM_funct3,
+    output reg [31:0] EX_MEM_Rdata2,
     output reg [31:0] EX_MEM_ALUResult,
-    output reg [31:0] EX_MEM_Rd_data,
-
+    output reg [31:0] EX_MEM_alu_exec_PC,
+    output reg EX_MEM_alu_exec_done,
+    output reg [7:0] EX_MEM_alu_physical_address,
     output reg [31:0] mul_exec_value,
-    output reg mul_exec_done,
     output reg [31:0] mul_exec_PC,
-
+    output reg mul_exec_done,
     output reg [31:0] div_exec_value,
-    output reg div_exec_done,
-    output reg [31:0] div_exec_PC
+    output reg [31:0] div_exec_PC,
+    output reg div_exec_done 
 );
         
     always @(posedge clk or posedge reset) begin
         if (reset) begin
             // 리셋 신호가 활성화되면 초기화
-            EX_MEM_RegWrite <= 1'b0;
             EX_MEM_MemToReg <= 1'b0;
             EX_MEM_MemRead <= 1'b0;
             EX_MEM_MemWrite <= 1'b0;
-            EX_MEM_RWsel <= 1'b0;
             EX_MEM_funct3 <= 3'b000;
-            EX_MEM_Rd <= 5'b00000;
+            EX_MEM_Rdata2 <= 32'b0;
             EX_MEM_ALUResult <= 32'b0;
-            EX_MEM_RData2 <= 32'b0;
-            EX_MEM_Rd_data <= 32'b0;
+            EX_MEM_alu_exec_PC <= 32'b0;
+            EX_MEM_alu_exec_done <= 1'b0;
+            EX_MEM_alu_physical_address <= 8'b00000000;
+            mul_exec_value <= 32'b0;
+            mul_exec_PC <= 32'b0;
+            mul_exec_done <= 1'b0;
+            div_exec_value <= 32'b0;
+            div_exec_PC <= 32'b0;
+            div_exec_done <= 1'b0; 
         end else begin
             // 정상 동작
-            EX_MEM_RegWrite <= ID_EX_RegWrite;
             EX_MEM_MemToReg <= ID_EX_MemToReg;
             EX_MEM_MemRead <= ID_EX_MemRead;
-            EX_MEM_MemWrite <= ID_EX_MemWrite;
-            EX_MEM_Rd <= ID_EX_Rd;
-            EX_MEM_funct3 <= ID_EX_funct3;
-            EX_MEM_ALUResult <= ALUResult;
-            EX_MEM_RData2 <= ID_EX_RData2;
-            EX_MEM_Rd_data <= Rd_data;
+            EX_MEM_MemWrite <= ID_EX_MemWrite; 
+            EX_MEM_funct3 <= RS_EX_funt3;
+            EX_MEM_Rdata2 <= operand2_Phy_Data;
+            EX_MEM_ALUResult <= RS_EX_ALUResult;
+            EX_MEM_alu_exec_PC <= RS_EX_PC_ALU;
+            EX_MEM_alu_exec_done <= ALU_done;
+            EX_MEM_alu_physical_address <= RS_EX_alu_Physical_address;
+            mul_exec_value <= Mul_Result;
+            mul_exec_PC <= RS_EX_PC_Mul_out;
+            mul_exec_done <= Mul_done_out;
+            div_exec_value <= Div_Result;
+            div_exec_PC <= RS_EX_PC_Div_out;
+            div_exec_done <= Div_done_out;
         end
     end
 endmodule
@@ -194,16 +212,14 @@ module memwb_pipeline_register (
     input EX_MEM_MemToReg,
     input [31:0] EX_MEM_ALUResult,
     input [31:0] RData, // data memory
-    input EX_MEM_alu_done,
-    input [31:0]EX_MEM_alu_exec_index,
-    input [4:0] EX_MEM_Rd,
-    output reg MEM_WB_MemToReg,
+    input [31:0] EX_MEM_alu_exec_PC,
+    input EX_MEM_alu_exec_done,
     
+    output reg MEM_WB_MemToReg,
     output reg [31:0] MEM_WB_ALUResult,
     output reg [31:0] MEM_WB_RData,
     output reg alu_exec_done,
     output reg [31:0] alu_exec_PC
-
 );
         
     always @(posedge clk or posedge reset) begin
@@ -219,11 +235,8 @@ module memwb_pipeline_register (
             MEM_WB_MemToReg <= EX_MEM_MemToReg;
             MEM_WB_ALUResult <= EX_MEM_ALUResult;
             MEM_WB_RData <= RData;
+            alu_exec_done <= EX_MEM_alu_exec_done;
             alu_exec_PC <= EX_MEM_alu_exec_PC;
-            if (!mem_read) begin
-                alu_exec_done <= EX_MEM_alu_done;
-            end else begin 
-                alu_exec_done <= 1'b1; 
             end
         end
    
