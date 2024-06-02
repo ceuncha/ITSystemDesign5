276af9962df037ffb6d3ea931d04bca94c65aed1
hominki <165993704+hominki@users.noreply.github.com>
2024-06-02 20:52:02 +0900

    Update address_mapper.v

--- Source file/address_mapper.v
+++ Source file/address_mapper.v
@@ -23,21 +23,25 @@ always @(*) begin
                 case(funct3)
                     3'b000: mapped_address = (funct7 == 7'b0000000) ? 6'd1 : // ADD
                                             (funct7 == 7'b0100000) ? 6'd2 : // SUB
-                                            6'd63; // 
+                                            (funct7 == 7'b0000001) ? 6'd27 : // MUL
+                                            6'd63;
                     3'b111: mapped_address = 6'd3; // AND
                     3'b110: mapped_address = 6'd4; // OR
                     3'b100: mapped_address = 6'd5; // XOR
                     3'b001: mapped_address = 6'd6; // SLL
                     3'b101: mapped_address = (funct7 == 7'b0000000) ? 6'd7 : // SRL
                                             (funct7 == 7'b0100000) ? 6'd8 : // SRA
-                                            6'd63; // 예외 처리
+                                            6'd63;
                     3'b010: mapped_address = 6'd9; // SLT
                     3'b011: mapped_address = 6'd10; // SLTU
+                    3'b100: mapped_address = (funct7 == 7'b0000001) ? 6'd28 : // DIV (몫)
+                                            (funct7 == 7'b0000000) ? 6'd29 : // REM (나머지)
+                                            6'd63;
                     default: mapped_address = 6'd63;
                 endcase
             end
             // Load
-            7'b0000011: mapped_address = 6'd11; //lad
+            7'b0000011: mapped_address = 6'd11; // 로드
             // Store 
             7'b0100011: mapped_address = 6'd12; 
             // B-type Branch operations
