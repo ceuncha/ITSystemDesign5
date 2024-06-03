e2b8263efe6a0881c0c7067c4849cd464e5d1ebc
hominki <165993704+hominki@users.noreply.github.com>
2024-06-03 15:07:17 +0900

    Update phsical_register_file.v

--- Source file/phsical_register_file.v
+++ Source file/phsical_register_file.v
@@ -3,7 +3,7 @@ module physical_register_file (
     input reset,
     input [7:0] Operand1_phy,
     input [7:0] Operand2_phy,
-    input [7:0] Rd_phy, // 명령어의 Rd 주소
+    input [7:0] Rd_phy, // 紐낅졊�뼱�쓽 Rd 二쇱냼
 
     input ALU_add_Write,
     input ALU_load_Write,
@@ -24,7 +24,7 @@ module physical_register_file (
     output reg valid2
 );
 
-// 레지스터와 valid 비트 저장
+// �젅吏��뒪�꽣�� valid 鍮꾪듃 ���옣
 reg [31:0] registers [0:255];
     reg valid [0:255];
 integer i;
@@ -33,34 +33,34 @@ always @(posedge reset) begin
    if (reset) begin
        for (i = 0; i < 32; i = i + 1) begin
             registers[i] <= i;
-            valid[i] <= 1'b1; // 초기 valid 비트는 1로 설정
+            valid[i] <= 1'b1; // 珥덇린 valid 鍮꾪듃�뒗 1濡� �꽕�젙
         end
-       for (i = 32; i < 255; i = i + 1) begin
+       for (i = 32; i < 256; i = i + 1) begin
             registers[i] <= 32'h00000000;
-           valid[i] <= 1'b1; // 초기 valid 비트는 0으로 설정
+           valid[i] <= 1'b1; // 珥덇린 valid 鍮꾪듃�뒗 0�쑝濡� �꽕�젙
         end
     end
 end
 
 
-// 클락 또는 리셋 신호 처리
+// �겢�씫 �삉�뒗 由ъ뀑 �떊�샇 泥섎━
 always @(posedge clk) begin
 
         if (ALU_add_Write == 1'b1 && ALU_add_phy != 7'b0) begin
             registers[ALU_add_phy] <= ALU_add_Data;
-            valid[ALU_add_phy] <= 1'b1; // validation 값 1로 설정
+            valid[ALU_add_phy] <= 1'b1; // validation 媛� 1濡� �꽕�젙
         end
         if (ALU_load_Write == 1'b1 && ALU_load_phy != 7'b0) begin
             registers[ALU_load_phy] <= ALU_load_Data;
-            valid[ALU_load_phy] <= 1'b1; // validation 값 1로 설정
+            valid[ALU_load_phy] <= 1'b1; // validation 媛� 1濡� �꽕�젙
         end
         if (ALU_mul_Write == 1'b1 && ALU_mul_phy != 7'b0) begin
             registers[ALU_mul_phy] <= ALU_mul_Data;
-            valid[ALU_mul_phy] <= 1'b1; // validation 값 1로 설정
+            valid[ALU_mul_phy] <= 1'b1; // validation 媛� 1濡� �꽕�젙
         end
         if (ALU_div_Write == 1'b1 && ALU_div_phy != 7'b0) begin
             registers[ALU_div_phy] <= ALU_div_Data;
-            valid[ALU_div_phy] <= 1'b1; // validation 값 1로 설정
+            valid[ALU_div_phy] <= 1'b1; // validation 媛� 1濡� �꽕�젙
         end
 
 end
@@ -71,7 +71,7 @@ always @(Rd_phy) begin
     end
 end
 
-// 읽기 연산
+// �씫湲� �뿰�궛
 always @(Operand1_phy or Operand2_phy) begin
     Operand1_data <= registers[Operand1_phy];
     Operand2_data <= registers[Operand2_phy];
