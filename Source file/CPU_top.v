94e03d7ae47031b82fab77ed4a7a79727392ef2d
hominki <165993704+hominki@users.noreply.github.com>
2024-06-05 20:48:34 +0900

    Update CPU_top.v

--- Source file/CPU_top.v
+++ Source file/CPU_top.v
@@ -280,6 +280,7 @@ RAT u_RAT(
     .clk(clk),
     .reset(rst),
 
+    .if_id_flush(PCSrc),
     .save_state(save_on),    // 사본 레지스터에 상태 저장 신호
     .restore_state(restore_on), // 사본 레지스터에서 상태 복원 신호
     .save_page(save_page),     // 상태 저장용 사본 레지스터 페이지 선택 신호
