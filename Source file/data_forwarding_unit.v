19862a6a354b8f2cd6ade50b0f300b6f4b41eb3e
kjs928 <164368225+kjs928@users.noreply.github.com>
2024-06-09 17:54:35 +0900

    Update data_forwarding_unit.v

--- Source file/data_forwarding_unit.v
+++ Source file/data_forwarding_unit.v
@@ -1,15 +1,13 @@
-module data_forwarding_unit(MEM_WB_RegWrite,EX_MEM_RegWrite,
-EX_MEM_Rd,ID_EX_Rs1,MEM_WB_Rd,ID_EX_Rs2,ForwardA,ForwardB);
-
-    input  MEM_WB_RegWrite;
-    input  EX_MEM_RegWrite;
-    input  [4:0] EX_MEM_Rd;
-    input  [4:0] ID_EX_Rs1;
-    input  [4:0] MEM_WB_Rd;
-    input  [4:0] ID_EX_Rs2;
-    output reg [1:0] ForwardA;
-    output reg [1:0] ForwardB;
-
+module data_forwarding_unit(
+    input MEM_WB_RegWrite,
+    input EX_MEM_RegWrite,
+    input [4:0] EX_MEM_Rd,
+    input [4:0] ID_EX_Rs1,
+    input [4:0] MEM_WB_Rd,
+    input [4:0] ID_EX_Rs2,
+    output reg [1:0] ForwardA,
+    output reg [1:0] ForwardB
+);
 
     // EX hazard
     always @(*) begin
@@ -24,15 +22,16 @@ EX_MEM_Rd,ID_EX_Rs1,MEM_WB_Rd,ID_EX_Rs2,ForwardA,ForwardB);
         if (EX_MEM_RegWrite && (EX_MEM_Rd != 0) && (EX_MEM_Rd == ID_EX_Rs2)) begin
             ForwardB = 2'b10;
         end
-   // Forwarding conditions for MEM hazard
+
+        // Forwarding conditions for MEM hazard
         if (MEM_WB_RegWrite && (MEM_WB_Rd != 0) && 
-            !((EX_MEM_RegWrite && (EX_MEM_Rd != 0) && (EX_MEM_Rd != ID_EX_Rs1))) &&
+            !((EX_MEM_RegWrite && (EX_MEM_Rd != 0) && (EX_MEM_Rd == ID_EX_Rs1))) &&
             (MEM_WB_Rd == ID_EX_Rs1)) begin
             ForwardA = 2'b01;
         end
-   //double data hazard 때문에 조건이 하나 더 붙음 (EX와 MEM 둘다 발생시 EX가 먼저라)
+        
         if (MEM_WB_RegWrite && (MEM_WB_Rd != 0) && 
-            !((EX_MEM_RegWrite && (EX_MEM_Rd != 0) && (EX_MEM_Rd != ID_EX_Rs1))) &&
+            !((EX_MEM_RegWrite && (EX_MEM_Rd != 0) && (EX_MEM_Rd == ID_EX_Rs2))) &&
             (MEM_WB_Rd == ID_EX_Rs2)) begin
             ForwardB = 2'b01;
         end
