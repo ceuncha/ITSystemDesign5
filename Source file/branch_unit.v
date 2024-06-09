dc219edbd76df231c379a3381f7769dc678f11a9
kjs928 <164368225+kjs928@users.noreply.github.com>
2024-06-09 20:04:19 +0900

    Update branch_unit.v

--- Source file/branch_unit.v
+++ Source file/branch_unit.v
@@ -5,7 +5,7 @@ module branch_unit(
     input ID_EX_Branch,         // 분기 활성화 신호
     input [2:0] ID_EX_funct3,   // 분기 유형을 결정하는 함수 코드
     input [31:0] ALUResult,     // ALU 결과 (분기 주소 계산에 사용)
-    input ID_EX_Jump,                 // 점프 활성화 신호
+    input ID_EX_Jump,           // 점프 활성화 신호
     output reg PCSrc,           // PC 소스 선택 신호 (분기 또는 점프 수행 시 1)
     output reg [31:0] PC_Branch,// 새로운 분기 또는 점프 주소
     output reg [31:0] Rd_data,  // Rd 레지스터에 저장할 데이터
@@ -35,8 +35,8 @@ always @(*) begin
             3'b001: PCSrc = (ResultA != ResultB);   // BNE
             3'b100: PCSrc = ($signed(ResultA) < $signed(ResultB));  // BLT
             3'b101: PCSrc = ($signed(ResultA) >= $signed(ResultB)); // BGE
-            3'b110: PCSrc = ($unsigned(ResultA) < $unsigned(ResultB)); // BLTU
-            3'b111: PCSrc = ($unsigned(ResultA) >= $unsigned(ResultB)); // BGEU
+            3'b110: PCSrc = (ResultA < ResultB); // BLTU (unsigned comparison)
+            3'b111: PCSrc = (ResultA >= ResultB); // BGEU (unsigned comparison)
             default: PCSrc = 0;
         endcase
 
