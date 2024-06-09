a5e646618fde7a9434153b48f5b4bf592f309927
kjs928 <164368225+kjs928@users.noreply.github.com>
2024-06-09 20:00:58 +0900

    Update Hazard_Detection_unit.v

--- Source file/Hazard_Detection_unit.v
+++ Source file/Hazard_Detection_unit.v
@@ -1,8 +1,7 @@
-module Hazard_Detection_unit(ID_EX_MemRead,MemRead,IF_ID_Rs1,IF_ID_Rs2,ID_EX_Rd,
+module Hazard_Detection_unit(ID_EX_MemRead,IF_ID_Rs1,IF_ID_Rs2,ID_EX_Rd,
 					PC_Stall,IF_ID_Stall,Control_Sig_Stall);
 														 
     input ID_EX_MemRead;
-    input MemRead;
     input [4:0] IF_ID_Rs1;
     input [4:0] IF_ID_Rs2;
     input [4:0] ID_EX_Rd;
@@ -23,7 +22,7 @@ module Hazard_Detection_unit(ID_EX_MemRead,MemRead,IF_ID_Rs1,IF_ID_Rs2,ID_EX_Rd,
         Control_Sig_Stall = 1'b0;
         
         // Hazard detection condition
-        if (((ID_EX_MemRead && !(MemRead)) && ((ID_EX_Rd == IF_ID_Rs1) ||(ID_EX_Rd == IF_ID_Rs2)))) begin
+        if ((ID_EX_MemRead && ((ID_EX_Rd == IF_ID_Rs1) ||(ID_EX_Rd == IF_ID_Rs2)))) begin
             PC_Stall = 1'b1;
             IF_ID_Stall = 1'b1;
             Control_Sig_Stall = 1'b1;
