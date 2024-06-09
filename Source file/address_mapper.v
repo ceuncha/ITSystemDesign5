40536d8b7105533609e953808285b4c6144fefcf
kjs928 <164368225+kjs928@users.noreply.github.com>
2024-06-09 20:04:03 +0900

    Update address_mapper.v

--- Source file/address_mapper.v
+++ Source file/address_mapper.v
@@ -12,28 +12,28 @@ end
 always @(*) begin
     case(opcode)
         // New operation at address 0
-        7'bXXXXXXX: mapped_address = 6'd0;
+        
         
         // R-type
         7'b0110011: begin
             case(funct3)
                 3'b000: mapped_address = (funct7 == 7'b0000000) ? 6'd1 : // ADD
                                         (funct7 == 7'b0100000) ? 6'd2 : // SUB
-                                        6'd63; // 
+                                        6'd0; // default case
                 3'b111: mapped_address = 6'd3; // AND
                 3'b110: mapped_address = 6'd4; // OR
                 3'b100: mapped_address = 6'd5; // XOR
                 3'b001: mapped_address = 6'd6; // SLL
                 3'b101: mapped_address = (funct7 == 7'b0000000) ? 6'd7 : // SRL
                                         (funct7 == 7'b0100000) ? 6'd8 : // SRA
-                                        6'd63; // 예외 처리
+                                        6'd0; // default case
                 3'b010: mapped_address = 6'd9; // SLT
                 3'b011: mapped_address = 6'd10; // SLTU
-                default: mapped_address = 6'd63;
+                default: mapped_address = 6'd0; // default case
             endcase
         end
         // Load
-        7'b0000011: mapped_address = 6'd11; //lad
+        7'b0000011: mapped_address = 6'd11; // load
         // Store 
         7'b0100011: mapped_address = 6'd12; 
         // B-type Branch operations
@@ -50,8 +50,8 @@ always @(*) begin
                 3'b001: mapped_address = 6'd20; // SLLI
                 3'b101: mapped_address = (funct7 == 7'b0000000) ? 6'd21 : // SRLI
                                         (funct7 == 7'b0100000) ? 6'd22 : // SRAI
-                                        6'd63;
-                default: mapped_address = 6'd63;
+                                        6'd0; // default case
+                default: mapped_address = 6'd0; // default case
             endcase
         end
         // LUI
@@ -62,7 +62,7 @@ always @(*) begin
         7'b1101111: mapped_address = 6'd25;
         // JALR
         7'b1100111: mapped_address = 6'd26;
-        default: mapped_address = 6'd63; 
+        default: mapped_address = 6'd0; // default case
     endcase
 end
 
