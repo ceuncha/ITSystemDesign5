99910378cf36498c7b2c02194bb67f5412d62509
ceuncha <155208795+ceuncha@users.noreply.github.com>
2024-06-12 16:38:35 +0900

    Update branch_unit.v

--- Source file/branch_unit.v
+++ Source file/branch_unit.v
@@ -1,13 +1,16 @@
 module branch_unit(
+    //input clk,
+    //input reset,
     input [31:0] ResultA,       // 첫 번째 비교 대상
     input [31:0] ResultB,       // 두 번째 비교 대상
+    input [31:0] IF_ID_PC,
     input [31:0] ID_EX_PC,      // 현재 명령어 주소
     input ID_EX_Branch,         // 분기 활성화 신호
     input [2:0] ID_EX_funct3,   // 분기 유형을 결정하는 함수 코드
     input [31:0] ALUResult,     // ALU 결과 (분기 주소 계산에 사용)
-    input ID_EX_BPred,
-    input ID_EX_BPredValid,
-    input ID_EX_Jump,                 // 점프 활성화 신호
+    input wire IF_ID_BPred,
+    input wire IF_ID_BPredValid,
+    input wire ID_EX_Jump,                 // 점프 활성화 신호
     output reg PCSrc,           // PC 소스 선택 신호 (분기 또는 점프 수행 시 1)
     output reg [31:0] PC_Branch,// 새로운 분기 또는 점프 주소
     output reg [31:0] Rd_data,  // Rd 레지스터에 저장할 데이터
@@ -15,6 +18,16 @@ module branch_unit(
     output reg ID_EX_Flush      // ID/EX 파이프라인 레지스터 플러시
 );
 
+wire prediction;
+assign prediction = (IF_ID_BPredValid && (IF_ID_BPred == PCSrc)) ? 1 : 0;
+//assign prediction = (IF_ID_PC == PC_Branch) ? 1 : 0;
+
+/*
+assign PC_Branch = ALUResult;
+assign PCSrc = ID_EX_Jump ? 1 :
+                (ID_EX_funct3 == 3'b000 && (ResultA == ResultB)) ? 1 :
+                ()
+*/
 always @(*) begin
     // 기본 설정
     PCSrc = 0;
@@ -43,22 +56,20 @@ always @(*) begin
             3'b111: PCSrc = ($unsigned(ResultA) >= $unsigned(ResultB)); // BGEU
             default: PCSrc = 0;
         endcase
-
         if (PCSrc) begin
             // 분기가 활성화되었을 때 PC 주소 변경, 플러시 신호 발생
             PC_Branch = ALUResult;
-            $display("Time: %0t | in branch unit, for PC: %h Branch to %h", $time, ID_EX_PC, PC_Branch);
-            /*
-            if (ID_EX_BPredValid && (ID_EX_BPred == PCSrc)) begin // if prediction was right
+            if (prediction) begin // if prediction was right
                 // do not flush the pipeline
-                IF_ID_Flush <= 0;
-                ID_EX_Flush <= 0;
-            $display("Time: %0t | in branch unit, for PC: %h prediction was right", $time, ID_EX_PC);
+                //IF_ID_Flush = 0;
+                ID_EX_Flush = 0;
+                $display("Time: %0t | in branch unit, for PC: %h prediction was right", $time, ID_EX_PC);
             end else begin
-                IF_ID_Flush <= 1;
-                ID_EX_Flush <= 1;
+                //IF_ID_Flush = 1;
+                ID_EX_Flush = 1;
+                $display("Time: %0t | in branch unit, for PC: %h prediction was wrong", $time, ID_EX_PC);
             end
-            */
+            //$display("Time: %0t | in branch unit, for PC: %h Branch to %h", $time, ID_EX_PC, PC_Branch)
         end
     end
 end
