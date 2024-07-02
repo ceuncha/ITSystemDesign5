785cd965c5153fedb47b8a404bafa317fa28fdda
jjurijang <165987742+jjurijang@users.noreply.github.com>
2024-07-02 21:09:37 +0900

    Update branch_unit.v

--- Source file/branch_unit.v
+++ Source file/branch_unit.v
@@ -1,4 +1,3 @@
-
 module BranchUnit(
     input wire ID_EX_Jump,
     input wire ID_EX_Branch,
@@ -10,24 +9,22 @@ module BranchUnit(
     input wire ALUNegative, // Negative flag from ALU
     input wire ALUZero,     // Zero flag from ALU
     input wire ALUOverflow, // Overflow flag from ALU
-    input wire ALUCarry,     // Carry flag from ALU
+    input wire ALUCarry,    // Carry flag from ALU
 
     output reg [31:0] PC_Branch,
     output reg [31:0] branch_index,
     output reg PCSrc,
-    output reg IF_ID_Flush
+    output reg IF_ID_Flush,
     output reg Predict_Result
 );
 
-
 always @(*) begin
         PCSrc = 0;
-        PC_Branch = 0;  // 湲곕낯 遺꾧린 二쇱냼
+        PC_Branch = 0;  // 기본 분기 주소
         IF_ID_Flush = 0;
-   
         if(ID_EX_Jump) begin
-            PC_Branch = ALUResult; // ?젏?봽 泥섎━
-            PCSrc = 1; // ?젏?봽 ?떆 PC ?냼?뒪 ?떊?샇 ?솢?꽦?솕
+            PC_Branch = ALUResult; // 점프 처리
+            PCSrc = 1; // 점프 시 PC 소스 신호 활성화
             IF_ID_Flush = 1'b1;
             branch_index = PC;
         end
@@ -39,17 +36,18 @@ always @(*) begin
             3'b101: PCSrc = ~ALUNegative; // BGE: Branch if Negative flag is 0
             3'b110: PCSrc = ALUCarry; // BLTU: Branch if Carry flag is 1
             3'b111: PCSrc = ~ALUCarry; // BGEU: Branch if Carry flag is 0
-
-            default: PCSrc = 0;
+            
+            default: PCSrc = 0; 
             endcase
             if(PCSrc) begin
-                PC_Branch = imm + PC + 4; // 遺꾧린 二쇱냼 ?뾽?뜲?씠?듃
+                PC_Branch = imm + PC + 4; // 분기 주소 업데이트
                 IF_ID_Flush = 1'b1;
                 branch_index = PC;
             end
         end
-
+        
+        // Prediction_Result 값 설정
+        Predict_Result = RS_EX_taken ^ PCSrc;
     end
-
 
 endmodule
