827d24d61a664e96c8652bed05a618b9fcced615
hominki <165993704+hominki@users.noreply.github.com>
2024-07-03 15:02:01 +0900

    Update RS_EX_Decoder.v

--- Source file/RS_EX_Decoder.v
+++ Source file/RS_EX_Decoder.v
@@ -1,13 +1,13 @@
 module RS_EX_decoder(
-    input clk,              // 클럭 신호
-    input reset,            // 리셋 신호
+    input clk,              // �겢�윮 �떊�샇
+    input reset,            // 由ъ뀑 �떊�샇
 
-    input [6:0] in_opcode,  // 7-bit, 연산 코드
-    input [31:0] in_operand1,  // 32-bit, 첫 번째 피연산자
-    input [31:0] in_operand2,  // 32-bit, 두 번째 피연산자
-    input [2:0] in_func3,   // 3-bit, 부가적 연산 정보
-    input [6:0] in_funct7,  // 7-bit, 부가적 연산 정보 (funct7 추가)
-    input [31:0] in_pc,     // 32-bit, 프로그램 카운터
+    input [6:0] in_opcode,  // 7-bit, �뿰�궛 肄붾뱶
+    input [31:0] in_operand1,  // 32-bit, 泥� 踰덉㎏ �뵾�뿰�궛�옄
+    input [31:0] in_operand2,  // 32-bit, �몢 踰덉㎏ �뵾�뿰�궛�옄
+    input [2:0] in_func3,   // 3-bit, 遺�媛��쟻 �뿰�궛 �젙蹂�
+    input [6:0] in_funct7,  // 7-bit, 遺�媛��쟻 �뿰�궛 �젙蹂� (funct7 異붽�)
+    input [31:0] in_pc,     // 32-bit, �봽濡쒓렇�옩 移댁슫�꽣
         
     input wire MemToReg,	
     input wire MemRead,
@@ -18,18 +18,19 @@ module RS_EX_decoder(
     input wire Jump,		
     input wire Branch,
     input wire IF_ID_taken,
+    input wire IF_ID_hit,
     
-    input [7:0] rd_phy_reg, // 8-bit, 물리적 레지스터 주소
+    input [7:0] rd_phy_reg, // 8-bit, 臾쇰━�쟻 �젅吏��뒪�꽣 二쇱냼
     input [7:0] Operand1_phy,  
     input [7:0] Operand2_phy,
     input [1:0] valid,
     input [31:0] immediate,
     input [31:0] inst_num,
 
-    output reg [31:0] add_alu_operand1,  // Add ALU로 보낼 첫 번째 피연산자
-    output reg [31:0] add_alu_operand2,  // Add ALU로 보낼 두 번째 피연산자
-    output reg [2:0] add_alu_func3,      // Add ALU로 보낼 func3
-    output reg [31:0] add_alu_pc,        // Add ALU로 보낼 프로그램 카운터
+    output reg [31:0] add_alu_operand1,  // Add ALU濡� 蹂대궪 泥� 踰덉㎏ �뵾�뿰�궛�옄
+    output reg [31:0] add_alu_operand2,  // Add ALU濡� 蹂대궪 �몢 踰덉㎏ �뵾�뿰�궛�옄
+    output reg [2:0] add_alu_func3,      // Add ALU濡� 蹂대궪 func3
+    output reg [31:0] add_alu_pc,        // Add ALU濡� 蹂대궪 �봽濡쒓렇�옩 移댁슫�꽣
 
     output reg out_add_MemToReg,	
     output reg out_add_MemRead,
@@ -38,9 +39,9 @@ module RS_EX_decoder(
     output reg out_add_ALUSrc1,		
     output reg out_add_ALUSrc2,		
     output reg out_add_Jump,		
-    output reg out_add_Branch,      // Add ALU로 보낼 제어 신호
+    output reg out_add_Branch,      // Add ALU濡� 蹂대궪 �젣�뼱 �떊�샇
 
-    output reg [7:0] add_rd_phy_reg,     // Add ALU로 보낼 물리적 레지스터 주소
+    output reg [7:0] add_rd_phy_reg,     // Add ALU濡� 蹂대궪 臾쇰━�쟻 �젅吏��뒪�꽣 二쇱냼
     output reg add_rs_on,
     output reg [7:0] out_add_Operand1_phy,
     output reg [7:0] out_add_Operand2_phy,
@@ -48,10 +49,10 @@ module RS_EX_decoder(
     output reg [31:0] out_add_immediate,
     output reg [31:0] out_add_inst_num,
     
-    output reg [31:0] mul_alu_operand1,  // Mul ALU로 보낼 첫 번째 피연산자
-    output reg [31:0] mul_alu_operand2,  // Mul ALU로 보낼 두 번째 피연산자
-    output reg [2:0] mul_alu_func3,      // Mul ALU로 보낼 func3
-    output reg [31:0] mul_alu_pc,        // Mul ALU로 보낼 프로그램 카운터
+    output reg [31:0] mul_alu_operand1,  // Mul ALU濡� 蹂대궪 泥� 踰덉㎏ �뵾�뿰�궛�옄
+    output reg [31:0] mul_alu_operand2,  // Mul ALU濡� 蹂대궪 �몢 踰덉㎏ �뵾�뿰�궛�옄
+    output reg [2:0] mul_alu_func3,      // Mul ALU濡� 蹂대궪 func3
+    output reg [31:0] mul_alu_pc,        // Mul ALU濡� 蹂대궪 �봽濡쒓렇�옩 移댁슫�꽣
 
     output reg out_mul_MemToReg,	
     output reg out_mul_MemRead,
@@ -60,9 +61,9 @@ module RS_EX_decoder(
     output reg out_mul_ALUSrc1,		
     output reg out_mul_ALUSrc2,		
     output reg out_mul_Jump,		
-    output reg out_mul_Branch,      // Add ALU로 보낼 제어 신호     
+    output reg out_mul_Branch,      // Add ALU濡� 蹂대궪 �젣�뼱 �떊�샇     
     
-    output reg [7:0] mul_rd_phy_reg,     // Mul ALU로 보낼 물리적 레지스터 주소
+    output reg [7:0] mul_rd_phy_reg,     // Mul ALU濡� 蹂대궪 臾쇰━�쟻 �젅吏��뒪�꽣 二쇱냼
     output reg mul_rs_on,
     output reg [7:0] out_mul_Operand1_phy,
     output reg [7:0] out_mul_Operand2_phy,
@@ -70,10 +71,10 @@ module RS_EX_decoder(
     output reg [31:0] out_mul_immediate,
     output reg [31:0] out_mul_inst_num,
     
-    output reg [31:0] div_alu_operand1,  // Div ALU로 보낼 첫 번째 피연산자
-    output reg [31:0] div_alu_operand2,  // Div ALU로 보낼 두 번째 피연산자
-    output reg [2:0] div_alu_func3,      // Div ALU로 보낼 func3
-    output reg [31:0] div_alu_pc,        // Div ALU로 보낼 프로그램 카운터
+    output reg [31:0] div_alu_operand1,  // Div ALU濡� 蹂대궪 泥� 踰덉㎏ �뵾�뿰�궛�옄
+    output reg [31:0] div_alu_operand2,  // Div ALU濡� 蹂대궪 �몢 踰덉㎏ �뵾�뿰�궛�옄
+    output reg [2:0] div_alu_func3,      // Div ALU濡� 蹂대궪 func3
+    output reg [31:0] div_alu_pc,        // Div ALU濡� 蹂대궪 �봽濡쒓렇�옩 移댁슫�꽣
 
     output reg out_div_MemToReg,	
     output reg out_div_MemRead,
@@ -84,19 +85,20 @@ module RS_EX_decoder(
     output reg out_div_Jump,		
     output reg out_div_Branch,     
 
-    output reg [7:0] div_rd_phy_reg,      // Div ALU로 보낼 물리적 레지스터 주소
+    output reg [7:0] div_rd_phy_reg,      // Div ALU濡� 蹂대궪 臾쇰━�쟻 �젅吏��뒪�꽣 二쇱냼
     output reg div_rs_on,
     output reg [7:0] out_div_Operand1_phy,
     output reg [7:0] out_div_Operand2_phy,
     output reg [1:0] out_div_valid,
     output reg [31:0] out_div_immediate,
     output reg [31:0] out_div_inst_num,
-    output reg RS_alu_IF_ID_taken
+    output reg RS_alu_IF_ID_taken,
+    output reg RS_alu_IF_ID_hit
 );
 
 always @(posedge clk or posedge reset) begin
     if (reset) begin
-        // 모든 출력을 리셋
+        // 紐⑤뱺 異쒕젰�쓣 由ъ뀑
         add_alu_operand1 <= 0;
         add_alu_operand2 <= 0;
         add_alu_func3 <= 0;
@@ -162,6 +164,7 @@ always @(posedge clk or posedge reset) begin
         out_div_inst_num <= 0; 
 
         RS_alu_IF_ID_taken <= 0;
+        RS_alu_IF_ID_hit <= 0;
     end
 end
 
@@ -172,10 +175,10 @@ always @(*) begin
                             mul_rs_on = 0;
                             div_rs_on = 0;
         end
-        7'b0110011: begin // R-type 명령어
+        7'b0110011: begin // R-type 紐낅졊�뼱
             case (in_funct7)
                 7'b0000001: begin
-                    // MUL, DIV, REM 명령어 처리
+                    // MUL, DIV, REM 紐낅졊�뼱 泥섎━
                     case (in_func3)
                         3'b000: begin // MUL
                             mul_alu_operand1= in_operand1;
@@ -250,7 +253,7 @@ always @(*) begin
                             out_div_inst_num = inst_num;
                         end
                         default: begin
-                            // 다른 R-type 명령어는 ADD ALU로 보내기
+                            // �떎瑜� R-type 紐낅졊�뼱�뒗 ADD ALU濡� 蹂대궡湲�
                             add_alu_operand1 = in_operand1;
                             add_alu_operand2 = in_operand2;
                             add_alu_func3 = in_func3;
@@ -274,11 +277,12 @@ always @(*) begin
                             out_add_Branch = Branch;
                             out_add_inst_num = inst_num;
                             RS_alu_IF_ID_taken = IF_ID_taken;
+                            RS_alu_IF_ID_hit = IF_ID_hit;
                         end
                     endcase
                 end
                 default: begin
-                    // 다른 R-type 명령어는 ADD ALU로 보내기
+                    // �떎瑜� R-type 紐낅졊�뼱�뒗 ADD ALU濡� 蹂대궡湲�
                     add_alu_operand1 = in_operand1;
                     add_alu_operand2 = in_operand2;
                     add_alu_func3 = in_func3;
@@ -302,11 +306,12 @@ always @(*) begin
                     out_add_Branch = Branch;
                     out_add_inst_num = inst_num;
                     RS_alu_IF_ID_taken = IF_ID_taken;
+                    RS_alu_IF_ID_hit = IF_ID_hit;
                 end
             endcase
         end
         default: begin
-            // 기본적으로 모든 명령어를 ADD ALU로 보냄
+            // 湲곕낯�쟻�쑝濡� 紐⑤뱺 紐낅졊�뼱瑜� ADD ALU濡� 蹂대깂
             add_alu_operand1 = in_operand1;
             add_alu_operand2 = in_operand2;
             add_alu_func3 = in_func3;
@@ -330,6 +335,7 @@ always @(*) begin
             out_add_Branch = Branch;
             out_add_inst_num = inst_num;
             RS_alu_IF_ID_taken = IF_ID_taken;
+            RS_alu_IF_ID_hit = IF_ID_hit;
         end
     endcase
 end
