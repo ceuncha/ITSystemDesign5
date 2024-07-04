0aebaec39a91212fe879b3073fd315013c63c906
jjurijang <165987742+jjurijang@users.noreply.github.com>
2024-07-05 00:06:21 +0900

    Update Program_Counter.v

--- Source file/Program_Counter.v
+++ Source file/Program_Counter.v
@@ -1,21 +1,25 @@
 module Program_Counter(
     input wire clk,
-    input wire reset, // 동기식 리셋 신호 추가
+    input wire reset, // ?룞湲곗떇 由ъ뀑 ?떊?샇 異붽?
     input wire [31:0] PC_final_next,
+    input wire taken,
+    output reg PC_taken,
     output reg [31:0] PC
 );
 
 initial begin
-    PC = 32'd0; // PC 초기값 설정
+    PC = 32'd0; // PC 珥덇린媛? ?꽕?젙
+    PC_taken = 0;
 end
 
 always @(posedge clk) begin
     if (reset) begin
-        // reset 신호가 활성화된 경우 PC 초기화
+        // reset ?떊?샇媛? ?솢?꽦?솕?맂 寃쎌슦 PC 珥덇린?솕
         PC <= 32'd0;
     end else begin
-        // PC를 PC_final_next 값으로 업데이트
+        // PC瑜? PC_final_next 媛믪쑝濡? ?뾽?뜲?씠?듃
         PC <= PC_final_next;
+        PC_taken <= taken;
     end
 end
 
