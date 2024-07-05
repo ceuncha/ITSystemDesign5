15036e3852880a0ed154d827d1f9a2a45dbcf478
hominki <165993704+hominki@users.noreply.github.com>
2024-07-05 23:58:49 +0900

    Update RS_EX_Decoder.v

--- Source file/RS_EX_Decoder.v
+++ Source file/RS_EX_Decoder.v
@@ -39,8 +39,8 @@ module RS_EX_decoder(
     output reg [3:0] out_add_ALUOP,
     output reg out_add_ALUSrc1,      
     output reg out_add_ALUSrc2,      
-    output reg out_add_Jump,      
-    output reg out_add_Branch,      // Add ALU濡? 蹂대궪 ?젣?뼱 ?떊?샇
+   
+ 
 
     output reg [7:0] add_rd_phy_reg,     // Add ALU濡? 蹂대궪 臾쇰━?쟻 ?젅吏??뒪?꽣 二쇱냼
     output reg add_rs_on,
@@ -55,14 +55,10 @@ module RS_EX_decoder(
     output reg [2:0] mul_alu_func3,      // Mul ALU濡? 蹂대궪 func3
     output reg [31:0] mul_alu_pc,        // Mul ALU濡? 蹂대궪 ?봽濡쒓렇?옩 移댁슫?꽣
 
-    output reg out_mul_MemToReg,   
-    output reg out_mul_MemRead,
-    output reg out_mul_MemWrite,   
+  
     output reg [3:0] out_mul_ALUOP,
-    output reg out_mul_ALUSrc1,      
-    output reg out_mul_ALUSrc2,      
-    output reg out_mul_Jump,      
-    output reg out_mul_Branch,      // Add ALU濡? 蹂대궪 ?젣?뼱 ?떊?샇     
+      
+   
     
     output reg [7:0] mul_rd_phy_reg,     // Mul ALU濡? 蹂대궪 臾쇰━?쟻 ?젅吏??뒪?꽣 二쇱냼
     output reg mul_rs_on,
@@ -77,14 +73,9 @@ module RS_EX_decoder(
     output reg [2:0] div_alu_func3,      // Div ALU濡? 蹂대궪 func3
     output reg [31:0] div_alu_pc,        // Div ALU濡? 蹂대궪 ?봽濡쒓렇?옩 移댁슫?꽣
 
-    output reg out_div_MemToReg,   
-    output reg out_div_MemRead,
-    output reg out_div_MemWrite,   
+   
     output reg [3:0] out_div_ALUOP,
-    output reg out_div_ALUSrc1,      
-    output reg out_div_ALUSrc2,      
-    output reg out_div_Jump,      
-    output reg out_div_Branch,     
+     
 
     output reg [7:0] div_rd_phy_reg,      // Div ALU濡? 蹂대궪 臾쇰━?쟻 ?젅吏??뒪?꽣 二쇱냼
     output reg div_rs_on,
@@ -94,78 +85,90 @@ module RS_EX_decoder(
     output reg [31:0] out_div_immediate,
     output reg [31:0] out_div_inst_num,
     output reg RS_alu_IF_ID_taken,
-    output reg RS_alu_IF_ID_hit
+    output reg RS_alu_IF_ID_hit,
+
+    output reg RS_br_Jump,
+    output reg RS_br_Branch,  
+    output reg RS_br_IF_ID_hit,
+    output reg RS_br_IF_ID_taken,    
+    output reg [2:0] RS_br_func3,
+    output reg [7:0] br_rd_phy_reg,
+    output reg RS_br_start,
+    output reg [7:0] RS_br_operand1,
+    output reg [7:0] RS_br_operand2,
+    output reg [7:0] RS_br_operand1_phy,
+    output reg [7:0] RS_br_operand2_phy,
+    output reg [7:0] RS_br_phy_reg,
+    output reg [1:0] RS_br_valid,
+    output reg [31:0] RS_br_immediate,
+    output reg [31:0] RS_br_inst_num,
+    output reg [31:0] RS_br_PC
+
+
+
 );
 
 always @(posedge clk or posedge reset) begin
     if (reset) begin
         // 紐⑤뱺 異쒕젰?쓣 由ъ뀑
-        add_alu_operand1 <= 0;
-        add_alu_operand2 <= 0;
-        add_alu_func3 <= 0;
-        add_alu_pc <= 0;
+        add_alu_operand1 = 0;
+        add_alu_operand2 = 0;
+        add_alu_func3 = 0;
+        add_alu_pc = 0;
 
-        add_rd_phy_reg <= 0;
-        out_add_Operand1_phy <= 0;
-        out_add_Operand2_phy <= 0;
-        out_add_valid <= 0;
-        out_add_inst_num<=0;
+        add_rd_phy_reg = 0;
+        out_add_Operand1_phy = 0;
+        out_add_Operand2_phy = 0;
+        out_add_valid = 0;
+        out_add_inst_num =0;
         
-        mul_alu_operand1 <= 0;
-        mul_alu_operand2 <= 0;
-        mul_alu_func3 <= 0;
-        mul_alu_pc <= 0;
+        mul_alu_operand1 = 0;
+        mul_alu_operand2 = 0;
+        mul_alu_func3 = 0;
+        mul_alu_pc = 0;
 
 
-        mul_rd_phy_reg <= 0;
-        out_mul_Operand1_phy <= 0;
-        out_mul_Operand2_phy <= 0;
-        out_mul_valid <= 0;
-        out_mul_inst_num<=0;
+        mul_rd_phy_reg = 0;
+        out_mul_Operand1_phy = 0;
+        out_mul_Operand2_phy = 0;
+        out_mul_valid = 0;
+        out_mul_inst_num =0;
 
-        div_alu_operand1 <= 0;
-        div_alu_operand2 <= 0;
-        div_alu_func3 <= 0;
-        div_alu_pc <= 0;
+        div_alu_operand1 = 0;
+        div_alu_operand2 = 0;
+        div_alu_func3 = 0;
+        div_alu_pc = 0;
 
-        div_rd_phy_reg <= 0;
-        out_div_Operand1_phy <= 0;
-        out_div_Operand2_phy <= 0;
-        out_div_valid <= 0;
-        add_rs_on <= 0;
-        mul_rs_on <= 0;
-        div_rs_on <= 0;
+        div_rd_phy_reg = 0;
+        out_div_Operand1_phy = 0;
+        out_div_Operand2_phy = 0;
+        out_div_valid = 0;
+        add_rs_on = 0;
+        mul_rs_on = 0;
+        div_rs_on = 0;
+        RS_br_start = 0;
 
-        out_add_MemToReg <= 0;   
-        out_add_MemRead <= 0;   
-        out_add_MemWrite <= 0;      
-        out_add_ALUOP <= 0;   
-        out_add_ALUSrc1 <= 0;
-        out_add_ALUSrc2 <= 0;      
-        out_add_Jump <= 0;      
-        out_add_Branch <= 0;  
+        out_add_MemToReg = 0;   
+        out_add_MemRead = 0;   
+        out_add_MemWrite = 0;      
+        out_add_ALUOP = 0;   
+        out_add_ALUSrc1 = 0;
+        out_add_ALUSrc2 = 0;      
+  
 
-        out_mul_MemToReg <= 0;   
-        out_mul_MemRead <= 0;   
-        out_mul_MemWrite <= 0;      
-        out_mul_ALUOP <= 0;   
-        out_mul_ALUSrc1 <= 0;
-        out_mul_ALUSrc2 <= 0;      
-        out_mul_Jump <= 0;      
-        out_mul_Branch <= 0;  
+     
+        out_mul_ALUOP = 0;   
+    
 
-        out_div_MemToReg <= 0;   
-        out_div_MemRead <= 0;   
-        out_div_MemWrite <= 0;      
-        out_div_ALUOP <= 0;   
-        out_div_ALUSrc1 <= 0;
-        out_div_ALUSrc2 <= 0;      
-        out_div_Jump <= 0;      
-        out_div_Branch <= 0; 
-        out_div_inst_num <= 0; 
 
-        RS_alu_IF_ID_taken <= 0;
-        RS_alu_IF_ID_hit <= 0;
+     
+        out_div_ALUOP = 0;   
+    
+ 
+        out_div_inst_num = 0; 
+
+        RS_br_IF_ID_taken = 0;
+        RS_br_IF_ID_hit = 0; 
     end
 end
 
@@ -175,6 +178,7 @@ always @(*) begin
                             add_rs_on = 0;
                             mul_rs_on = 0;
                             div_rs_on = 0;
+                            RS_br_start = 0;                            
         end
         7'b0110011: begin // R-type 紐낅졊?뼱
             case (in_funct7)
@@ -191,18 +195,13 @@ always @(*) begin
                             add_rs_on = 0;
                             mul_rs_on = 1;
                             div_rs_on = 0;
+                            RS_br_start = 0;
                             out_mul_Operand1_phy = Operand1_phy;  
                             out_mul_Operand2_phy  = Operand2_phy;
                             out_mul_valid = valid;
                             out_mul_immediate = immediate;
-                            out_mul_MemToReg = MemToReg;   
-                            out_mul_MemRead = MemRead;   
-                            out_mul_MemWrite = MemWrite;      
-                            out_mul_ALUOP = ALUOP;   
-                            out_mul_ALUSrc1 = ALUSrc1;
-                            out_mul_ALUSrc2 = ALUSrc2;      
-                            out_mul_Jump = Jump;      
-                            out_mul_Branch = Branch;
+     
+   
                             out_mul_inst_num = inst_num;
                         end
                         3'b100: begin // DIV
@@ -215,18 +214,14 @@ always @(*) begin
                             add_rs_on = 0;
                             mul_rs_on = 0;
                             div_rs_on = 1;
+                            RS_br_start = 0;
                             out_div_Operand1_phy = Operand1_phy;  
                             out_div_Operand2_phy = Operand2_phy;
                             out_div_valid = valid;
                             out_div_immediate = immediate;
-                            out_div_MemToReg = MemToReg;   
-                            out_div_MemRead = MemRead;   
-                            out_div_MemWrite = MemWrite;      
-                            out_div_ALUOP = ALUOP;   
-                            out_div_ALUSrc1 = ALUSrc1;
-                            out_div_ALUSrc2 = ALUSrc2;      
-                            out_div_Jump = Jump;      
-                            out_div_Branch = Branch;
+                            out_div_ALUOP = ALUOP; 
+                             
+
                             out_div_inst_num = inst_num;
                         end
                         3'b110: begin // REM
@@ -239,18 +234,14 @@ always @(*) begin
                             add_rs_on = 0;
                             mul_rs_on = 0;
                             div_rs_on = 1;
+                            RS_br_start = 0;
                             out_div_Operand1_phy = Operand1_phy;  
                             out_div_Operand2_phy = Operand2_phy;
                             out_div_valid = valid;
                             out_div_immediate = immediate;
-                            out_div_MemToReg = MemToReg;   
-                            out_div_MemRead = MemRead;   
-                            out_div_MemWrite = MemWrite;      
-                            out_div_ALUOP = ALUOP;   
-                            out_div_ALUSrc1 = ALUSrc1;
-                            out_div_ALUSrc2 = ALUSrc2;      
-                            out_div_Jump = Jump;      
-                            out_div_Branch = Branch;
+                            out_div_ALUOP = ALUOP; 
+
+
                             out_div_inst_num = inst_num;
                         end
                         default: begin
@@ -264,6 +255,7 @@ always @(*) begin
                             add_rs_on = 1;
                             mul_rs_on = 0;
                             div_rs_on = 0;
+                            RS_br_start = 0;
                             out_add_Operand1_phy = Operand1_phy;
                             out_add_Operand2_phy = Operand2_phy;
                             out_add_valid = valid;
@@ -274,11 +266,9 @@ always @(*) begin
                             out_add_ALUOP = ALUOP;   
                             out_add_ALUSrc1 = ALUSrc1;
                             out_add_ALUSrc2 = ALUSrc2;      
-                            out_add_Jump = Jump;      
-                            out_add_Branch = Branch;
+ 
                             out_add_inst_num = inst_num;
-                             RS_alu_IF_ID_taken = 0;
-                            RS_alu_IF_ID_hit = 0;    
+   
                         end
                     endcase
                 end
@@ -293,6 +283,7 @@ always @(*) begin
                     add_rs_on = 1;
                     mul_rs_on = 0;
                     div_rs_on = 0;
+                    RS_br_start = 0;
                     out_add_Operand1_phy = Operand1_phy;
                     out_add_Operand2_phy = Operand2_phy;
                     out_add_valid = valid;
@@ -303,16 +294,39 @@ always @(*) begin
                     out_add_ALUOP = ALUOP;   
                     out_add_ALUSrc1 = ALUSrc1;
                     out_add_ALUSrc2 = ALUSrc2;      
-                    out_add_Jump = Jump;      
-                    out_add_Branch = Branch;
+   
                     out_add_inst_num = inst_num;
-                             RS_alu_IF_ID_taken = 0;
-                            RS_alu_IF_ID_hit = 0;    
+  
                 end
             endcase
         end
         default: begin
-            // 湲곕낯?쟻?쑝濡? 紐⑤뱺 紐낅졊?뼱瑜? ADD ALU濡? 蹂대깂
+        if (in_opcode == 7'b1101111 || in_opcode == 7'b1100111 || in_opcode == 7'b1100011) begin
+            RS_br_operand1 = in_operand1;
+            RS_br_operand2 = in_operand2;
+            RS_br_func3 = in_func3;
+            RS_br_PC = in_pc;
+
+            RS_br_phy_reg = rd_phy_reg;
+
+            add_rs_on = 0;
+            mul_rs_on = 0;
+            div_rs_on = 0;
+            RS_br_start = 1;
+
+            RS_br_operand1_phy = Operand1_phy;
+            RS_br_operand2_phy = Operand2_phy;
+            RS_br_valid = valid;     
+            RS_br_Jump = Jump;      
+            RS_br_Branch = Branch;
+            RS_br_inst_num = inst_num;
+            RS_br_IF_ID_taken = IF_ID_taken;
+            RS_br_IF_ID_hit = IF_ID_hit; 
+
+            end else begin
+    
+
+            
             add_alu_operand1 = in_operand1;
             add_alu_operand2 = in_operand2;
             add_alu_func3 = in_func3;
@@ -322,6 +336,7 @@ always @(*) begin
             add_rs_on = 1;
             mul_rs_on = 0;
             div_rs_on = 0;
+            RS_br_start = 0;
             out_add_Operand1_phy = Operand1_phy;
             out_add_Operand2_phy = Operand2_phy;
             out_add_valid = valid;
@@ -332,16 +347,10 @@ always @(*) begin
             out_add_ALUOP = ALUOP;   
             out_add_ALUSrc1 = ALUSrc1;
             out_add_ALUSrc2 = ALUSrc2;      
-            out_add_Jump = Jump;      
-            out_add_Branch = Branch;
+
             out_add_inst_num = inst_num;
-                            if (in_opcode == 7'b1101111 || in_opcode == 7'b1100111 || in_opcode == 7'b1100011) begin
-                                    RS_alu_IF_ID_taken = IF_ID_taken;
-                                     RS_alu_IF_ID_hit = IF_ID_hit;
-                            end else begin
-                                     RS_alu_IF_ID_taken = 1'b0;
-                                     RS_alu_IF_ID_hit = 1'b0;
-                            end
+
+            end
         end
     endcase
 end
