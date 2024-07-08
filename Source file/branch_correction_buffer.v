edc7141c70ed52508645a206a40a558198d5e528
jjurijang <165987742+jjurijang@users.noreply.github.com>
2024-07-09 00:53:48 +0900

    Update branch_correction_buffer.v

--- Source file/branch_correction_buffer.v
+++ Source file/branch_correction_buffer.v
@@ -21,7 +21,7 @@ reg [2:0] next_index; // Next index to insert new entry
 
 // Temporary index variable
 reg [2:0] find_index;
-
+reg [2:0] find_index2;
 // Reset all entries
 always @(posedge clk) begin
     if (reset) begin
@@ -58,7 +58,6 @@ always @(posedge clk) begin
         pc_reverse[6] <= 32'd0;
         pc_reverse[7] <= 32'd0;
         next_index <= 3'd0; // Initialize next_index
-        Wrong <= 1'b0; // Initialize Wrong signal
         
     end else begin
         // Write operation
@@ -111,20 +110,20 @@ end
 // Read operation
 always @(*) begin
     
-    find_index = 3'd7; // Default to an invalid index
-    if (valid[0] && (pc[0] == ID_EX_PC)) find_index = 3'd0;
-    else if (valid[1] && (pc[1] == ID_EX_PC)) find_index = 3'd1;
-    else if (valid[2] && (pc[2] == ID_EX_PC)) find_index = 3'd2;
-    else if (valid[3] && (pc[3] == ID_EX_PC)) find_index = 3'd3;
-    else if (valid[4] && (pc[4] == ID_EX_PC)) find_index = 3'd4;
-    else if (valid[5] && (pc[5] == ID_EX_PC)) find_index = 3'd5;
-    else if (valid[6] && (pc[6] == ID_EX_PC)) find_index = 3'd6;
-    else if (valid[7] && (pc[7] == ID_EX_PC)) find_index = 3'd7;
+    find_index2 = 3'd7; // Default to an invalid index
+    if (valid[0] && (pc[0] == ID_EX_PC)) find_index2 = 3'd0;
+    else if (valid[1] && (pc[1] == ID_EX_PC)) find_index2 = 3'd1;
+    else if (valid[2] && (pc[2] == ID_EX_PC)) find_index2 = 3'd2;
+    else if (valid[3] && (pc[3] == ID_EX_PC)) find_index2 = 3'd3;
+    else if (valid[4] && (pc[4] == ID_EX_PC)) find_index2 = 3'd4;
+    else if (valid[5] && (pc[5] == ID_EX_PC)) find_index2 = 3'd5;
+    else if (valid[6] && (pc[6] == ID_EX_PC)) find_index2 = 3'd6;
+    else if (valid[7] && (pc[7] == ID_EX_PC)) find_index2 = 3'd7;
     
     
-    if (find_index != 3'd7 && valid[find_index]) begin
-        PC_reverse <= pc_reverse[find_index];
-        if (taken_entry[find_index] == Pcsrc) begin
+    if (find_index2 != 3'd7 && valid[find_index2]) begin
+        PC_reverse <= pc_reverse[find_index2];
+        if (taken_entry[find_index2] == Pcsrc) begin
             Wrong = 1'b0; // Prediction is correct
         end else begin
             Wrong = 1'b1; // Prediction is incorrect
