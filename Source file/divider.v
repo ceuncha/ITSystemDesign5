873ef7568e2afdba3d00a6c9ea3f921a74df87cb
minseo20000 <165993774+minseo20000@users.noreply.github.com>
2024-07-09 23:32:09 +0900

    Update divider.v

--- Source file/divider.v
+++ Source file/divider.v
@@ -20,17 +20,25 @@ module divider (
     reg [31:0] PC_reg[0:31];
     reg [3:0] divider_op_reg[0:31];
     reg done_reg[0:31];
+    integer i;
 
     // 초기화 및 시작
-    always @(posedge clk or posedge reset) begin
-        if (reset) begin
-            temp_dividend[0] <= 64'd0;
-            divisor[0] <= 32'd0;
-            Physical_address_reg[0] <= 7'd0;
-            PC_reg[0] <= 32'd0;
-            divider_op_reg[0] <=1'd0;
-            done_reg[0] <= 1'd0;
-        end else if (start) begin
+
+
+    // 32 스테이지 생성
+    always @(posedge clk) begin
+    if (reset) begin
+        for (i = 0; i < 32; i = i + 1) begin
+            temp_dividend[i] <= 64'd0;
+            divisor[i] <= 32'd0;
+            Physical_address_reg[i] <= 8'd0;
+            PC_reg[i] <= 32'd0;
+            divider_op_reg[i] <= 1'd0;
+            done_reg[i] <= 1'd0;
+        end
+        end
+       else begin
+       if (start) begin
             temp_dividend[0] <= {31'b0, A, 1'b0};
             divisor[0] <= B;
             Physical_address_reg[0] <= Physical_address_in;
@@ -38,40 +46,25 @@ module divider (
             divider_op_reg[0] <= divider_op_in;
             done_reg[0] <= 1'd1;
         end
-    end
-
-    // 32 스테이지 생성
-    genvar i;
-    generate
-        for (i = 0; i < 31; i = i + 1) begin : stages
-            always @(posedge clk or posedge reset) begin
-                if (reset) begin
-                    temp_dividend[i+1] <= 64'd0;
-                    divisor[i+1] <= 32'd0;
-                    Physical_address_reg[i+1] <= 8'd0;
-                    PC_reg[i+1] <= 32'd0;
-                    divider_op_reg [i+1] <=1'd0; 
-                    done_reg[i+1] <= 1'd0;                   
-                end else begin
-                    if (temp_dividend[i][63:32] >= divisor[i]) begin
-                        temp_dividend[i][63:32] = temp_dividend[i][63:32] - divisor[i];
-                        temp_dividend[i][0] = 1'b1;
-                    end else begin
-                        temp_dividend[i][0] = 1'b0;
-                    end
-                    temp_dividend[i+1] <= {temp_dividend[i][62:0], 1'b0};
-                    divisor[i+1] <= divisor[i];
-                    Physical_address_reg[i+1] <= Physical_address_reg[i];
-                    PC_reg[i+1] <= PC_reg[i];
-                    divider_op_reg [i+1] <= divider_op_reg [i];
-                    done_reg[i+1] <= done_reg[i];
-                end
+        for (i = 0; i < 31; i = i + 1) begin
+            if (temp_dividend[i][63:32] >= divisor[i]) begin
+                temp_dividend[i][63:32] = temp_dividend[i][63:32] - divisor[i];
+                temp_dividend[i][0] = 1'b1;
+            end else begin
+                temp_dividend[i][0] = 1'b0;
             end
+            temp_dividend[i+1] <= {temp_dividend[i][62:0], 1'b0};
+            divisor[i+1] <= divisor[i];
+            Physical_address_reg[i+1] <= Physical_address_reg[i];
+            PC_reg[i+1] <= PC_reg[i];
+            divider_op_reg[i+1] <= divider_op_reg[i];
+            done_reg[i+1] <= done_reg[i];
         end
-    endgenerate
+        end
+        end
 
     // 결과 설정
-   always @(posedge clk or posedge reset) begin
+   always @(posedge clk) begin
         if(reset) begin
          Result <=0;
          Physical_address_out <= 0;
