a3e0f9030421924cc858b773e7ef19ec8f30f4db
minseo20000 <165993774+minseo20000@users.noreply.github.com>
2024-07-10 14:43:45 +0900

    Update control_rom.v

--- Source file/control_rom.v
+++ Source file/control_rom.v
@@ -1,6 +1,6 @@
 module control_rom(
     input [5:0] mapped_address,
-    input reset, // 리셋 신호 추가
+    input reset, // 由ъ뀑 �떊�샇 異붽�
     output RegWrite,
     output MemToReg,
     output MemRead,
@@ -9,19 +9,19 @@ module control_rom(
     output [1:0] ALUSrc,
     output RWsel,
     output Branch,
-    output Jump // 추가된 Jump 신호
+    output Jump // 異붽��맂 Jump �떊�샇
 );
 
-reg [12:0] ROM [0:63]; // ROM 크기를 13비트로 증가
+reg [12:0] ROM [0:63]; // ROM �겕湲곕�� 13鍮꾪듃濡� 利앷�
 
 assign {RegWrite, MemToReg, MemRead, MemWrite, ALUOp, ALUSrc, RWsel, Branch, Jump} = ROM[mapped_address];
 
     always @(posedge reset) begin
    
-        // 모든 제어 신호를 주소 0에서 0으로 초기화
+        // 紐⑤뱺 �젣�뼱 �떊�샇瑜� 二쇱냼 0�뿉�꽌 0�쑝濡� 珥덇린�솕
         ROM[0] = 13'b0_0_0_0_0000_00_0_0_0;
 
-        // R-타입 명령어
+        // R-���엯 紐낅졊�뼱
         ROM[1] = 13'b1_0_0_0_0010_10_0_0_0; // ADD
         ROM[2] = 13'b1_0_0_0_0110_10_0_0_0; // SUB
         ROM[3] = 13'b1_0_0_0_0000_10_0_0_0; // AND
@@ -33,14 +33,14 @@ assign {RegWrite, MemToReg, MemRead, MemWrite, ALUOp, ALUSrc, RWsel, Branch, Jum
         ROM[9] = 13'b1_0_0_0_1000_10_0_0_0; // SLT
         ROM[10] = 13'b1_0_0_0_1001_10_0_0_0; // SLTU
 
-        // 로드 및 스토어
-        ROM[11] = 13'b1_1_1_0_0010_11_0_0_0; // 로드
-        ROM[12] = 13'b0_0_0_1_0010_11_0_0_0; // 스토어
+        // 濡쒕뱶 諛� �뒪�넗�뼱
+        ROM[11] = 13'b1_1_1_0_0010_11_0_0_0; // 濡쒕뱶
+        ROM[12] = 13'b0_0_0_1_0010_11_0_0_0; // �뒪�넗�뼱
 
-        // Branch 명령어
-        ROM[13] = 13'b0_0_0_0_0110_10_0_1_0; // 분기 명령어에 대한 ALUOp 설정
+        // Branch 紐낅졊�뼱
+        ROM[13] = 13'b0_0_0_0_0110_10_0_1_0; // 遺꾧린 紐낅졊�뼱�뿉 ���븳 ALUOp �꽕�젙
 
-        // I-타입 즉시값 연산 및 ALU 명령어
+        // I-���엯 利됱떆媛� �뿰�궛 諛� ALU 紐낅졊�뼱
         ROM[14] = 13'b1_0_0_0_0010_11_0_0_0; // ADDI
         ROM[15] = 13'b1_0_0_0_1000_11_0_0_0; // SLTI
         ROM[16] = 13'b1_0_0_0_1001_11_0_0_0; // SLTIU
@@ -57,10 +57,10 @@ assign {RegWrite, MemToReg, MemRead, MemWrite, ALUOp, ALUSrc, RWsel, Branch, Jum
         ROM[25] = 13'b1_0_0_0_0010_01_1_0_1; // JAL
         ROM[26] = 13'b1_0_0_0_0010_11_1_0_1; // JALR
 
-        // 추가된 명령어
+        // 異붽��맂 紐낅졊�뼱
         ROM[27] = 13'b1_0_0_0_1010_10_0_0_0; // MUL
-        ROM[28] = 13'b1_0_0_0_0001_10_0_0_0; // DIV (몫)
-        ROM[29] = 13'b1_0_0_0_0000_10_0_0_0; // REM (나머지)
+        ROM[28] = 13'b1_0_0_0_0001_10_0_0_0; // DIV (紐�)
+        ROM[29] = 13'b1_0_0_0_0000_10_0_0_0; // REM (�굹癒몄�)
 
 end
 
