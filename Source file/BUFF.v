0b6b530854d878ebc7ab1dcb53aba6bc08f09abc
hominki <165993704+hominki@users.noreply.github.com>
2024-07-12 21:22:36 +0900

    Update BUFF.v

--- Source file/BUFF.v
+++ Source file/BUFF.v
@@ -1,33 +1,13 @@
-`timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 2024/07/04 17:40:31
-// Design Name: 
-// Module Name: BUFF
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
 
 
 module BUFF(     input clk,
-input rst,                                          // 내보내줘야 한다. priority encodcr은 이 역할을 수행한다.
+input rst,                                          
     input PC_taken,
-    output reg real_taken // 64비트 Y 출력
+    output reg real_taken // 64鍮꾪듃 Y 異쒕젰
     );
  always @(posedge clk) begin
         if (rst) begin
-            // 由ъ뀑 ?떊?샇媛? ?솢?꽦?솕?릺硫? 珥덇린?솕
+        
             real_taken <= 1'b0;
         end else begin
          real_taken <= PC_taken;
