f898e76a0b69dd2951739ebe7113744d3d894cd6
jjurijang <165987742+jjurijang@users.noreply.github.com>
2024-07-13 03:07:51 +0900

    Update ROB.v

--- Source file/ROB.v
+++ Source file/ROB.v
@@ -53,8 +53,8 @@ always @(posedge clk) begin
                 if (rob_entry[i][31:0] == branch_index) begin
                     rob_entry[i][98:0] <= {rob_entry[i][98], 1'b1, rob_entry[i][96], PC_Return, rob_entry[i][63:32], rob_entry[i][31:0]};
                     tail <= (i + 1) % 64; // Move tail to the entry right after the branch entry
-                    rob_entry[(i+1)%64][98:0] <= 99'bz; // Flush under tail entry
-                    rob_entry[(i+2)%64][98:0] <= 99'bz; // Fulsh under tail entry
+                    rob_entry[(i+1)%64][98] <= 0; // Flush under tail entry
+                    rob_entry[(i+2)%64][98] <= 0; // Fulsh under tail entry
   
                 end
             end
