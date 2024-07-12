b60e5a2928ae81c8a620f09853759a98824324f3
hominki <165993704+hominki@users.noreply.github.com>
2024-07-12 21:29:23 +0900

    Update phsical_register_file.v

--- Source file/phsical_register_file.v
+++ Source file/phsical_register_file.v
@@ -28,15 +28,15 @@ module physical_register_file (
 );
 
 
-reg [31:0] registers [0:255];
-    reg valid [0:255];
-integer i;
+   (* keep = "true" *) reg [31:0] registers [0:255];
+   (* keep = "true" *) reg valid [0:255];
+   (* keep = "true" *) integer i;
 
 
 
 
-always @(posedge clk) begin         // 매 클락 계산기들 혹은 load로부터 데이터값을 받는다. 전송온 데이터가 있다면, 해당 데이터를 업데이트 시켜준다.
-                                       // F0의 경우 항상 0을 유지해야 하므로 해당 물리주소는 업데이트 해주지 않는다,
+always @(posedge clk) begin         // 留? ?겢?씫 怨꾩궛湲곕뱾 ?샊?? load濡쒕??꽣 ?뜲?씠?꽣媛믪쓣 諛쏅뒗?떎. ?쟾?넚?삩 ?뜲?씠?꽣媛? ?엳?떎硫?, ?빐?떦 ?뜲?씠?꽣瑜? ?뾽?뜲?씠?듃 ?떆耳쒖??떎.
+                                       // F0?쓽 寃쎌슦 ?빆?긽 0?쓣 ?쑀吏??빐?빞 ?븯誘?濡? ?빐?떦 臾쇰━二쇱냼?뒗 ?뾽?뜲?씠?듃 ?빐二쇱? ?븡?뒗?떎,
 
    if (reset) begin
        for (i = 0; i < 32; i = i + 1) begin
@@ -48,25 +48,26 @@ always @(posedge clk) begin         // 매 클락 계산기들 혹은 load로부
            valid[i] <= 1'b1; 
         end
     end else begin
+    
         if (ALU_add_Write == 1'b1 && ALU_add_phy != 7'b0) begin
             registers[ALU_add_phy] <= ALU_add_Data;
-            valid[ALU_add_phy] <= 1'b1; // alu 신호가 들어왔을때 해당 물리주소와 데이터값을 받아서 pfile을 업데이트 시켜준다.
+            valid[ALU_add_phy] <= 1'b1; // alu ?떊?샇媛? ?뱾?뼱?솕?쓣?븣 ?빐?떦 臾쇰━二쇱냼?? ?뜲?씠?꽣媛믪쓣 諛쏆븘?꽌 pfile?쓣 ?뾽?뜲?씠?듃 ?떆耳쒖??떎.
         end
         if (ALU_load_Write == 1'b1 && ALU_load_phy != 7'b0) begin
             registers[ALU_load_phy] <= ALU_load_Data;
-            valid[ALU_load_phy] <= 1'b1; // load 신호가 들어왔을때 해당 물리주소와 데이터값을 받아서 pfile을 업데이트 시켜준다.
+            valid[ALU_load_phy] <= 1'b1; // load ?떊?샇媛? ?뱾?뼱?솕?쓣?븣 ?빐?떦 臾쇰━二쇱냼?? ?뜲?씠?꽣媛믪쓣 諛쏆븘?꽌 pfile?쓣 ?뾽?뜲?씠?듃 ?떆耳쒖??떎.
         end
         if (ALU_mul_Write == 1'b1 && ALU_mul_phy != 7'b0) begin
             registers[ALU_mul_phy] <= ALU_mul_Data;
-            valid[ALU_mul_phy] <= 1'b1; // multiplier 신호가 들어왔을때 해당 물리주소와 데이터값을 받아서 pfile을 업데이트 시켜준다.
+            valid[ALU_mul_phy] <= 1'b1; // multiplier ?떊?샇媛? ?뱾?뼱?솕?쓣?븣 ?빐?떦 臾쇰━二쇱냼?? ?뜲?씠?꽣媛믪쓣 諛쏆븘?꽌 pfile?쓣 ?뾽?뜲?씠?듃 ?떆耳쒖??떎.
         end
         if (ALU_div_Write == 1'b1 && ALU_div_phy != 7'b0) begin
             registers[ALU_div_phy] <= ALU_div_Data;
-            valid[ALU_div_phy] <= 1'b1; // divider 신호가 들어왔을때 해당 물리주소와 데이터값을 받아서 pfile을 업데이트 시켜준다.
+            valid[ALU_div_phy] <= 1'b1; // divider ?떊?샇媛? ?뱾?뼱?솕?쓣?븣 ?빐?떦 臾쇰━二쇱냼?? ?뜲?씠?꽣媛믪쓣 諛쏆븘?꽌 pfile?쓣 ?뾽?뜲?씠?듃 ?떆耳쒖??떎.
         end
         if (ALU_done_Write == 1'b1 && ALU_done_phy != 7'b0) begin
             registers[ALU_done_phy] <= ALU_done_Data;
-            valid[ALU_done_phy] <= 1'b1; // multiplier 신호가 들어왔을때 해당 물리주소와 데이터값을 받아서 pfile을 업데이트 시켜준다.
+            valid[ALU_done_phy] <= 1'b1; // multiplier ?떊?샇媛? ?뱾?뼱?솕?쓣?븣 ?빐?떦 臾쇰━二쇱냼?? ?뜲?씠?꽣媛믪쓣 諛쏆븘?꽌 pfile?쓣 ?뾽?뜲?씠?듃 ?떆耳쒖??떎.
         end
         if (Rd_phy != 7'b0) begin
         valid[Rd_phy] <= 1'b0;
@@ -76,7 +77,7 @@ end
 
 
 
-    always @(*) begin    //Operand 값이 들어오면 해당 물리주소의 data 값과 valid 값을 출력시켜 준다.
+    always @(*) begin    //Operand 媛믪씠 ?뱾?뼱?삤硫? ?빐?떦 臾쇰━二쇱냼?쓽 data 媛믨낵 valid 媛믪쓣 異쒕젰?떆耳? 以??떎.
         Operand1_data = registers[Operand1_phy];   
         Operand2_data = registers[Operand2_phy];
         valid1 = valid[Operand1_phy];
