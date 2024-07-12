aa2cba3189ebeb1fa84e6751b82fdff2377c6223
hominki <165993704+hominki@users.noreply.github.com>
2024-07-12 15:02:31 +0900

    Update DataMemory.v

--- Source file/DataMemory.v
+++ Source file/DataMemory.v
@@ -15,7 +15,8 @@ reg [31:0] memory [0:2047];
        for (i = 0; i < 2047; i = i + 1) begin
             memory[i] <= i+3;
         end
-    end
+    end else begin
+    
     if (EX_MEM_MemWrite) begin
         case (EX_MEM_funct3)
             3'b000: memory[EX_MEM_ALUResult][7:0] <= EX_MEM_Rdata2[7:0]; // SB
@@ -26,24 +27,21 @@ reg [31:0] memory [0:2047];
     end
     else if (!EX_MEM_MemWrite) ; // If EX_MEM_MemWrite is false, no changes are made to memory - it retains its previous state
 end
-
+end
 always @ (*) begin
 // Default value for RData, ensures it is always assigned
     Load_Data <= 32'd0; // if MemRead is false
     if (Load_Done) begin
         case (EX_MEM_funct3)
-            3'b000: Load_Data <= {{24{memory[EX_MEM_ALUResult][31]}}, memory[EX_MEM_ALUResult][7:0]}; // LB
-            3'b001: Load_Data <= {{16{memory[EX_MEM_ALUResult][31]}}, memory[EX_MEM_ALUResult][15:0]}; // LH
-            3'b010: Load_Data <= memory[EX_MEM_ALUResult]; // LW
-            3'b100: Load_Data <= {{24{1'b0}}, memory[EX_MEM_ALUResult][7:0]}; // LBU
-            3'b101: Load_Data <= {{16{1'b0}}, memory[EX_MEM_ALUResult][15:0]}; // LHU
-            default: Load_Data <= 32'd0; // Default value assignment to handle cases when MemRead is false
+            3'b000: Load_Data = {{24{memory[EX_MEM_ALUResult][31]}}, memory[EX_MEM_ALUResult][7:0]}; // LB
+            3'b001: Load_Data = {{16{memory[EX_MEM_ALUResult][31]}}, memory[EX_MEM_ALUResult][15:0]}; // LH
+            3'b010: Load_Data = memory[EX_MEM_ALUResult]; // LW
+            3'b100: Load_Data = {{24{1'b0}}, memory[EX_MEM_ALUResult][7:0]}; // LBU
+            3'b101: Load_Data = {{16{1'b0}}, memory[EX_MEM_ALUResult][15:0]}; // LHU
+            default: Load_Data = 32'd0; // Default value assignment to handle cases when MemRead is false
         endcase
     end
 end
 
-always @ (*) begin
-
-end
 
 endmodule
