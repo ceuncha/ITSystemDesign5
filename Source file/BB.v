c15ca57a15fed094c737dd91302189907c8d150e
hominki <165993704+hominki@users.noreply.github.com>
2024-07-12 21:22:08 +0900

    Update BB.v

--- Source file/BB.v
+++ Source file/BB.v
@@ -20,10 +20,10 @@ integer i;
 integer j;
 reg [4:0] head;
 reg [4:0] tail;
-reg Paste_RAT_set;                    // ?뵆?옒洹? 異붽?
+reg Paste_RAT_set;                    // ?逾�?�삋域�? �빊遺�?
 
 // Reset BB entries
-task reset_bb_entries;  //?븳踰? 遺꾧린媛? 吏꾪뻾?릺硫?, ?씠?썑?쓽 遺꾧린 ?젙蹂대뒗 臾댁쓽誘명븯寃? ?릺誘?濡? bb瑜? 鍮꾩썙以??떎.
+task reset_bb_entries;  //?釉녘린? �겫袁㏓┛揶�? 筌욊쑵六�?由븝쭖?, ?�뵠?�뜎?�벥 �겫袁㏓┛ ?�젟癰귣��뮉 �눧�똻�벥沃섎챸釉�野�? ?由븃첋?嚥�? bb�몴? �뜮袁⑹뜖餓�??�뼄.
     begin
         head <= 0;
         tail <= 0;
@@ -40,10 +40,10 @@ always @(posedge clk) begin
         reset_bb_entries();
     end else begin
         // Check for jump or branch opcode
-        if (opcode == 7'b1100011 || opcode == 7'b1101111 || opcode == 7'b1100111) begin // branch 紐낅졊?뼱媛? inst memory?뿉?꽌 ?굹?삤寃? ?릺硫? 
-                                                                                           //counter ?닽?옄?? 
-                                                                                           //?럹?씠吏? ?꽆踰꾨?? BB?뿉 ???옣?빐以??떎. 肄붾뱶?뿉?뒗 pc?씪怨? 
-                                                                                           // ?쟻???엳吏?留? counter ?닽?옄瑜? ?엯?젰諛쏅뒗?떎.
+        if (opcode == 7'b1100011 || opcode == 7'b1101111 || opcode == 7'b1100111) begin // branch 筌뤿굝議�?堉긷첎? inst memory?肉�?苑� ?援�?�궎野�? ?由븝쭖? 
+                                                                                           //counter ?�떭?�쁽?? 
+                                                                                           //?�읂?�뵠筌�? ?苑녻린袁�?? BB?肉� ???�삢?鍮먧빳??�뼄. �굜遺얜굡?肉�?�뮉 pc?�뵬��? 
+                                                                                           // ?�읅???�뿳筌�?筌�? counter ?�떭?�쁽�몴? ?�뿯?�젾獄쏆룆�뮉?�뼄.
             BB_entry[tail] <= PC;    // Store the current PC value in ROB at tail position
             ready[tail] <= 1'b0;     // Set ready flag to 0
             tail_num <= tail;        //
@@ -54,27 +54,27 @@ always @(posedge clk) begin
         end
 
         // Compare branch_PC with head's PC and check PCSrc
-    if (RS_EX_Branch || RS_EX_Jump) begin                   /*branch?뿉?꽌 遺꾧린 紐낅졊?씠 ?닔?뻾?릺硫?, BB濡? 遺꾧린紐낅졊?씠 ?떆?뻾?릺?뿀?떎?뒗 ?떊?샇??  counter number?씠 ?쟾?넚?맂?떎.
-                                                            BB?뒗 ?쟻???엳?뜕 Branch ?떊?샇?뱾?쓽 counter ?닽?옄?? branch濡쒕??꽣 ?삩 counter ?닽?옄瑜? ?씠?슜?빐?꽌 ?빐?떦 ?뿏?듃由ъ쓽 ready瑜? 1濡? 蹂?寃쏀빐以??떎,
-                                                            留뚯빟 head?뿉 ?쐞移섑븳 紐낅졊?뼱?쓽 遺꾧린?떊?샇媛? ?뱾?뼱?솕?떎硫?, 蹂꾨룄?쓽 ready bit ?꽕?젙 ?뾾?씠 諛붾줈 蹂듦뎄 ?떊?샇瑜? rat?? freelist濡? ?쟾?넚?빐以??떎. */
-        if (BB_entry[head] == branch_PC) begin              // head?뿉 ?쐞移섑븳 遺꾧린 ?떊?샇媛? ?뱾?뼱?솕?쓣?븣
+    if (RS_EX_Branch || RS_EX_Jump) begin                   /*branch?肉�?苑� �겫袁㏓┛ 筌뤿굝議�?�뵠 ?�땾?六�?由븝쭖?, BB嚥�? �겫袁㏓┛筌뤿굝議�?�뵠 ?�뻻?六�?由�?肉�?�뼄?�뮉 ?�뻿?�깈??  counter number?�뵠 ?�읈?�꽊?留�?�뼄.
+                                                            BB?�뮉 ?�읅???�뿳?�쐲 Branch ?�뻿?�깈?諭�?�벥 counter ?�떭?�쁽?? branch嚥≪뮆??苑� ?�궔 counter ?�떭?�쁽�몴? ?�뵠?�뒠?鍮�?苑� ?鍮�?�뼣 ?肉�?�뱜�뵳�딆벥 ready�몴? 1嚥�? 癰�?野껋�鍮먧빳??�뼄,
+                                                            筌띾슣鍮� head?肉� ?�맄燁살꼹釉� 筌뤿굝議�?堉�?�벥 �겫袁㏓┛?�뻿?�깈揶�? ?諭�?堉�?�넅?�뼄筌�?, 癰귢쑬猷�?�벥 ready bit ?苑�?�젟 ?毓�?�뵠 獄쏅뗀以� 癰귣벀�럡 ?�뻿?�깈�몴? rat?? freelist嚥�? ?�읈?�꽊?鍮먧빳??�뼄. */
+        if (BB_entry[head] == branch_PC) begin              // head?肉� ?�맄燁살꼹釉� �겫袁㏓┛ ?�뻿?�깈揶�? ?諭�?堉�?�넅?�뱽?釉�
             if (PCSrc == 1'b1) begin
                 Paste_RAT <= 1;      // Set Paste_RAT to 1
-                Paste_RAT_set <= 1;  // ?뵆?옒洹? ?꽕?젙
+                Paste_RAT_set <= 1;  // ?逾�?�삋域�? ?苑�?�젟
                 head_num <= head;
                 reset_bb_entries();  // Reset BB entries
             end else begin
                 head <= (head + 1)% 32;    // Increment head
                 Paste_RAT <= 0;      // Reset Paste_RAT to 0
-                Paste_RAT_set <= 0;  // ?뵆?옒洹? 由ъ뀑
+                Paste_RAT_set <= 0;  // ?逾�?�삋域�? �뵳�딅��
             end
         end
         for (i = 0; i < 32; i = i + 1) begin
-            if (BB_entry[i] == branch_PC) begin //head?뿉 ?쐞移섑븯吏? ?븡?? 遺꾧린?떊?샇媛? ?뱾?뼱?솕?쓣?븣
+            if (BB_entry[i] == branch_PC) begin //head?肉� ?�맄燁살꼹釉�筌�? ?釉�?? �겫袁㏓┛?�뻿?�깈揶�? ?諭�?堉�?�넅?�뱽?釉�
                 if (PCSrc == 1'b1) begin
                 ready[i] <= 1'b1;   // Set the ready flag to 1
                 Paste_RAT <= 1;      // Set Paste_RAT to 1
-                Paste_RAT_set <= 1;  // ?뵆?옒洹? ?꽕?젙
+                Paste_RAT_set <= 1;  // ?逾�?�삋域�? ?苑�?�젟
                 head_num <= i;
                 tail <= i;
                     for (j = i; j < 32; j = j + 1) begin
@@ -91,12 +91,12 @@ always @(posedge clk) begin
 
 
         // Check if the head is ready
-        if (ready[head] == 1'b1) begin    //head媛? 蹂??븯???쓣?븣 ready bit媛? 1?씠硫?, 蹂듦뎄 ?떊?샇瑜? ?쟾?넚?빐以??떎.
+        if (ready[head] == 1'b1) begin    //head揶�? 癰�??釉�???�뱽?釉� ready bit揶�? 1?�뵠筌�?, 癰귣벀�럡 ?�뻿?�깈�몴? ?�읈?�꽊?鍮먧빳??�뼄.
 
             reset_bb_entries();  // Reset BB entries
         end
 
-        // ?뵆?옒洹몃?? ?솗?씤?븯?뿬 Paste_RAT 由ъ뀑
+        // ?逾�?�삋域밸챶?? ?�넇?�뵥?釉�?肉� Paste_RAT �뵳�딅��
         if (Paste_RAT_set == 1) begin
             Paste_RAT <= 0;
             Paste_RAT_set <= 0;
