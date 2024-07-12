da33b022edc406dbc42eebe2143283ab10e7d053
hominki <165993704+hominki@users.noreply.github.com>
2024-07-12 21:29:50 +0900

    Update sign_extend.v

--- Source file/sign_extend.v
+++ Source file/sign_extend.v
@@ -1,16 +1,16 @@
 module sign_extend(
-    input [31:0] inst,  // 전체 명령어 입력
-    input clk,          // 클락 신호 입력
-    output reg [31:0] Imm  // 신호 확장된 즉시값 출력
+    input [31:0] inst,  // �쟾泥� 紐낅졊�뼱 �엯�젰
+    input clk,          // �겢�씫 �떊�샇 �엯�젰
+    output reg [31:0] Imm  // �떊�샇 �솗�옣�맂 利됱떆媛� 異쒕젰
 );
 
-// Opcode는 명령어의 가장 낮은 7비트에 위치
+// Opcode�뒗 紐낅졊�뼱�쓽 媛��옣 �궙�� 7鍮꾪듃�뿉 �쐞移�
 wire [6:0] opcode = inst[6:0];
 
-// Opcode를 기반으로 명령어 유형 판별 및 적절한 즉시값 추출 및 신호 확장
+// Opcode瑜� 湲곕컲�쑝濡� 紐낅졊�뼱 �쑀�삎 �뙋蹂� 諛� �쟻�젅�븳 利됱떆媛� 異붿텧 諛� �떊�샇 �솗�옣
 always @(posedge clk) begin
     if (opcode == 7'b0000011 || opcode == 7'b0010011 || opcode == 7'b1100111) begin
-        // I-type (예: load, immediate, JALR)
+        // I-type (�삁: load, immediate, JALR)
         Imm[11:0] <= inst[31:20];
         Imm[31:12] <= {20{inst[31]}};
     end
@@ -26,13 +26,13 @@ always @(posedge clk) begin
         Imm[10:5] <= inst[30:25];
         Imm[4:1] <= inst[11:8];
         Imm[11] <= inst[7];
-        Imm[0] <= 1'b0; // B-type 즉시값은 항상 짝수
+        Imm[0] <= 1'b0; // B-type 利됱떆媛믪� �빆�긽 吏앹닔
         Imm[31:13] <= {19{inst[31]}};
     end
     else if (opcode == 7'b0110111 || opcode == 7'b0010111) begin
         // U-type (LUI, AUIPC)
         Imm[31:12] <= inst[31:12];
-        Imm[11:0] <= 12'b0; // 하위 12비트는 0
+        Imm[11:0] <= 12'b0; // �븯�쐞 12鍮꾪듃�뒗 0
     end
     else if (opcode == 7'b1101111) begin
         // J-type (JAL)
@@ -40,11 +40,11 @@ always @(posedge clk) begin
         Imm[10:1] <= inst[30:21];
         Imm[11] <= inst[20];
         Imm[19:12] <= inst[19:12];
-        Imm[0] <= 1'b0; // J-type 즉시값은 항상 짝수
+        Imm[0] <= 1'b0; // J-type 利됱떆媛믪� �빆�긽 吏앹닔
         Imm[31:21] <= {11{inst[31]}};
     end
     else begin
-        Imm <= 32'b0; // 즉시값이 없는 명령어 유형의 경우
+        Imm <= 32'b0; // 利됱떆媛믪씠 �뾾�뒗 紐낅졊�뼱 �쑀�삎�쓽 寃쎌슦
     end
 end
 
