56c44622f0880fa4dfe3f37727690993568d7c9d
jjurijang <165987742+jjurijang@users.noreply.github.com>
2024-07-12 14:56:30 +0900

    Update logical_register.v

--- Source file/logical_register.v
+++ Source file/logical_register.v
@@ -1,20 +1,20 @@
 module logical_address_register (
     input clk,
     input reset,
-    input Reg_write,        // MEM 단계에서 데이터 쓰기 신호
-    input [4:0] logical_address, // 논리 주소 (0번지부터 31번지)
-    input [31:0] write_data,    // MEM 단계에서 쓰기 데이터
+    input Reg_write,        // MEM �떒怨꾩뿉�꽌 �뜲�씠�꽣 �벐湲� �떊�샇
+    input [4:0] logical_address, // �끉由� 二쇱냼 (0踰덉�遺��꽣 31踰덉�)
+    input [31:0] write_data,    // MEM �떒怨꾩뿉�꽌 �벐湲� �뜲�씠�꽣
     output [31:0] x0, x1, x2, x3, x4, x5, x6, x7, x8, x9, x10, x11, x12, x13, x14, x15,
     output [31:0] x16, x17, x18, x19, x20, x21, x22, x23, x24, x25, x26, x27, x28, x29, x30, x31
 );
-    reg [31:0] logical_registers [0:31]; // 논리 주소 저장 레지스터
+    reg [31:0] logical_registers [0:31]; // �끉由� 二쇱냼 ���옣 �젅吏��뒪�꽣
     integer i;
 
-    // 클락 또는 리셋 신호 처리
-    always @(*) begin
+    // �겢�씫 �삉�뒗 由ъ뀑 �떊�샇 泥섎━
+    always @(posedge clk) begin
         if (reset) begin
             for (i = 0; i < 32; i = i + 1) begin
-                logical_registers[i] <= i; // 초기값은 번지수
+                logical_registers[i] <= i; // 珥덇린媛믪� 踰덉��닔
             end
         end else if (Reg_write) begin
             if (logical_address != 5'b0) begin
@@ -23,7 +23,7 @@ module logical_address_register (
         end
     end
 
-    // 모든 레지스터 값을 출력하는 부분
+    // 紐⑤뱺 �젅吏��뒪�꽣 媛믪쓣 異쒕젰�븯�뒗 遺�遺�
     assign x0 = logical_registers[0];
     assign x1 = logical_registers[1];
     assign x2 = logical_registers[2];
