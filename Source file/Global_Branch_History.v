efc0ee77c307ba2f93995bf433282214b1555644
minseo20000 <165993774+minseo20000@users.noreply.github.com>
2024-07-22 13:56:05 +0900

    Update Global_Branch_History.v

--- Source file/Global_Branch_History.v
+++ Source file/Global_Branch_History.v
@@ -6,12 +6,12 @@ module Global_History_Register (
     output reg [3:0] branch_history // 4-bit branch history
 );
 
-// 鍮꾨룞湲� 由ъ뀑 諛� 遺꾧린 �엳�뒪�넗由� �뾽�뜲�씠�듃
+// 비동기 리셋 및 분기 히스토리 업데이트
 always @(posedge clk ) begin
     if (reset) begin
-        branch_history <= 4'b0000; // 鍮꾨룞湲� 由ъ뀑
+        branch_history <= 4'b0000; // 비동기 리셋
     end else if (ID_EX_Branch) begin
-        // �깉濡쒖슫 遺꾧린 寃곌낵瑜� LSB�뿉 異붽��븯怨� �굹癒몄�瑜� �삤瑜몄そ�쑝濡� �씠�룞
+        // 새로운 분기 결과를 LSB에 추가하고 나머지를 오른쪽으로 이동
         branch_history <= {branch_history[2:0], Pcsrc};
     end
 end
