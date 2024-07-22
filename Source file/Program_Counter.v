6cb5ce8632faece2e4b0288885634be737521ab8
minseo20000 <165993774+minseo20000@users.noreply.github.com>
2024-07-22 13:58:02 +0900

    Update Program_Counter.v

--- Source file/Program_Counter.v
+++ Source file/Program_Counter.v
@@ -1,6 +1,6 @@
 module Program_Counter(
     input wire clk,
-    input wire reset, // ?猷욄묾怨쀫뻼 �뵳�딅�� ?�뻿?�깈 �빊遺�?
+    input wire reset, // ?룞湲곗떇 由ъ뀑 ?떊?샇 異붽?
     input wire [31:0] PC_final_next,
     input wire taken,
     output reg PC_taken,
@@ -8,16 +8,16 @@ module Program_Counter(
 );
 
 initial begin
-    PC = 32'd0; // PC �룯�뜃由겼첎? ?苑�?�젟
+    PC = 32'd0; // PC 珥덇린媛? ?꽕?젙
     PC_taken = 0;
 end
 
 always @(posedge clk) begin
     if (reset) begin
-        // reset ?�뻿?�깈揶�? ?�넞?苑�?�넅?留� 野껋럩�뒭 PC �룯�뜃由�?�넅
+        // reset ?떊?샇媛? ?솢?꽦?솕?맂 寃쎌슦 PC 珥덇린?솕
         PC <= 32'd0;
     end else begin
-        // PC�몴? PC_final_next 揶쏅�れ몵嚥�? ?毓�?�쑓?�뵠?�뱜
+        // PC瑜? PC_final_next 媛믪쑝濡? ?뾽?뜲?씠?듃
         PC <= PC_final_next;
         PC_taken <= taken;
     end
