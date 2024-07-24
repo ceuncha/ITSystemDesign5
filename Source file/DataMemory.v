956d19303bb74a35b1bb4bd7e425bb6ffeb5ea1a
jjurijang <165987742+jjurijang@users.noreply.github.com>
2024-07-24 13:43:23 +0900

    Update DataMemory.v

--- Source file/DataMemory.v
+++ Source file/DataMemory.v
@@ -2,8 +2,8 @@ module DataMemory(
     input wire clk,
     input wire reset,
     input wire LS_MemRead,
-    input wire LS_MemWrite
-    input wire [31:0] LS_inst_num
+    input wire LS_MemWrite,
+    input wire [31:0] LS_inst_num,
     input wire [2:0] funct3_LS,
     input wire [31:0] LS_Result,
     input wire [31:0] Operand2_LS,
@@ -37,39 +37,37 @@ end
     if (reset) begin
             Load_Data <= 32'd0; // Default value assignment to handle cases when Load_Done is false or EX_MEM_funct3 is not matched
             Load_inst_num <=  32'd0;
-            Load_Done <= 0
+            Load_Done <= 0;
     end else begin
-
     if (funct3_LS == 3'b000) begin
         Load_Data <= {{24{memory[LS_Result][31]}}, memory[LS_Result][7:0]}; // LB
         Load_inst_num <= LS_inst_num;
-        Load_Done <= Load_Done
+        Load_Done <= Load_Done;
         end else if (funct3_LS == 3'b001) begin
             Load_Data <= {{16{memory[LS_Result][31]}}, memory[LS_Result][15:0]}; // LH
             Load_inst_num <= LS_inst_num;
-            Load_Done <= Load_Done
+            Load_Done <= Load_Done;
         end else if (funct3_LS == 3'b010) begin
             Load_Data <= memory[LS_Result]; // LW
             Load_inst_num <= LS_inst_num;
-            Load_Done <= Load_Done
+            Load_Done <= Load_Done;
 
         end else if (funct3_LS == 3'b100) begin
             Load_Data <= {{24{1'b0}}, memory[LS_Result][7:0]}; // LBU
             Load_inst_num <= LS_inst_num;
-            Load_Done <= Load_Done
+            Load_Done <= Load_Done;
 
         end else if (funct3_LS == 3'b101) begin
             Load_Data <= {{16{1'b0}}, memory[LS_Result][15:0]}; // LHU
             Load_inst_num <= LS_inst_num;
-            Load_Done <= Load_Done
+            Load_Done <= Load_Done;
 
         end else begin
             Load_Data <= 32'd0; // Default value assignment to handle cases when Load_Done is false or EX_MEM_funct3 is not matched
             Load_inst_num <=  32'd0;
-            Load_Done <= 0
+            Load_Done <= 0;
 
         end
     end
-    end
-
+end
 endmodule
