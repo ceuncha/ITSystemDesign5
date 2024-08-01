9ba4ed91535d9ee98cce4566bc03d1ee8890d0f4
jjurijang <165987742+jjurijang@users.noreply.github.com>
2024-08-01 15:41:20 +0900

    Update exceptionbuffer.v

--- Source file/exceptionbuffer.v
+++ Source file/exceptionbuffer.v
@@ -1,33 +1,41 @@
 module ExceptionBuffer (
     input clk,
     input [31:0] ex_inst_num,
-    input [31:0] exception_pc,
-
+    input [31:0] EHR_PC,
+    input [31:0] excpetion_pc,
+    input [31:0] exception_cause,
+    
     input [31:0] inst_num_rob,
     input exception_on,
   
     output [31:0] handler_pc,
-
+    output [31:0] CSR_EPC
+    output [3:0] CSR_cause
 );
     reg [4:0] tail;
-    reg [31:0] pc [0:15];
+    reg [31:0] ehr_pc[0:15];
+    reg [31:0] epc [0:15];
     reg [31:0] inst_num [0:15];
-
+    reg [3:0] cause [0:15];
     // Exception buffer logic
     always @(posedge clk) begin
         if (rst) begin
             // Reset the buffer and write pointer
             for (i = 0; i < 16; i = i + 1) begin
-                pc[i] = 0;
-                inst_num[i]=0;                
+                epc[i] <= 0;
+                ehr_pc[i] <= 0;
+                inst_num[i] <= 0;
+                cause[i] <= 0;
             end
-        handler_pc = 0;
-         tail = 0;
+        handler_pc <= 0;
+         tail <= 0;
         end else begin
             if(exception_pc != 0) begin
-                pc[tail] = exception_pc;
-                inst_num[tail] = ex_inst_num;
-                tail = (tail  + 1)%16; 
+                epc[tail] <= exception_pc;
+                ehr_pc[tail] <= EHR_PC;
+                inst_num[tail] <= ex_inst_num;
+                cause[tail] <= excpetion_cause;
+                tail <= (tail  + 1)%16; 
             end
         end
     end
@@ -37,8 +45,9 @@ module ExceptionBuffer (
         if(exception_on) begin
             for (i = 0; i < 16; i = i + 1) begin
                 if (inst_num[i] == inst_num_rob) begin
-                    handler_pc = pc[i];
-                                 
+                    handler_pc <= ehr_pc[i];
+                    CSR_EPC <= epc[i];
+                    CSR_cause <= cause[i];
                 end
             end
         end
