6182624aae79ec2eb3e4434898750c5e01ff872c
jjurijang <165987742+jjurijang@users.noreply.github.com>
2024-08-01 15:42:51 +0900

    Update IVT.v

--- Source file/IVT.v
+++ Source file/IVT.v
@@ -10,22 +10,27 @@ module IVT(
     input Address_inst_num,
 
     output IVT_inst_num,
-    output reg [15:0] handler_address  // 예외 핸들러 주소
+    output reg [15:0] handler_address,  // 예외 핸들러 주소
+    output reg [3:0] e_cause
 );
 
 always @(posedge clk) begin
     if (illegal_instruction) begin
         handler_address <= 16'h02BC; // Illegal Instruction: 700번지
         IVT_inst_num <= IF_ID_inst_num;
+        e_cause <= 4'b0000;
     end else if (LS) begin
         handler_address <= 16'h02E4; // LS: 740번지
         IVT_inst_num <= LS_que_inst_num;
+        e_cause <=4'b0001;
     end else if (Div_0) begin
         handler_address <= 16'h030C; // DIV 0: 780번지
         IVT_inst_num <= Div_inst_num;
+        e_cause <=4'b0010;
     end else if (Address) begin
         handler_address <= 16'h0334; // Address: 820번지
         IVT_inst_num <= Address_inst_num;
+        e_cause <=4'b0011;
     end else begin
         handler_address <= 16'h0000; // Default: 0000번지
     end
