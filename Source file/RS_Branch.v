4dc51ac430d5ce7b506ba9ccfe8995c8da64f515
jjurijang <165987742+jjurijang@users.noreply.github.com>
2024-08-01 23:40:52 +0900

    Update RS_Branch.v

--- Source file/RS_Branch.v
+++ Source file/RS_Branch.v
@@ -89,7 +89,7 @@ module RS_Branch (                                             //紐낅졊?뼱 f
     wire operand2_BR_conflict = (operand2 == BR_Phy);
     wire operand2_P_conflict = (operand2 == P_Phy);
     wire operand1_CSR_conflict = (operand2 == CSR_Phy);
-    wire operand2_conflict = operand2_ALU_conflict || operand2_MUL_conflict || operand2_DIV_conflict || operand12_MEM_conflict || operand2_BR_conflict || operand2_P_conflict || operand1_CSR_conflict;
+    wire operand2_conflict = operand2_ALU_conflict || operand2_MUL_conflict || operand2_DIV_conflict || operand12_MEM_conflict || operand2_BR_conflict || operand2_P_conflict || operand2_CSR_conflict;
 
     always @(posedge clk) begin    //由ъ뀑?떊?샇濡? 珥덇린?솕 ?떆耳쒖쨲
         if (reset) begin
