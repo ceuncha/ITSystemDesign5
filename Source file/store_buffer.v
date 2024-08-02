66e30b2cf32327b6428d9783830f09f2c7781225
hominki <165993704+hominki@users.noreply.github.com>
2024-08-03 00:03:08 +0900

    Update store_buffer.v

--- Source file/store_buffer.v
+++ Source file/store_buffer.v
@@ -16,7 +16,7 @@ module store_buffer(
     output reg [31:0] load_data,
     output reg [7:0] load_phy_out,
     output reg [31:0] inst_num_out,
-    output reg load_valid,
+    output reg [1:0] load_valid,
     output reg [2:0] funct3_out,
     output reg load_done_out,
 
@@ -29,6 +29,7 @@ module store_buffer(
     reg [31:0] buffer_inst_num [0:SIZE-1];
     reg [31:0] buffer_mem_addr [0:SIZE-1];
     reg [31:0] buffer_mem_data [0:SIZE-1];
+    reg [2:0] funct3s [0:SIZE-1];
 
     reg [SIZE-1:0] entry_val;
     reg [4:0] current_block;
@@ -78,10 +79,13 @@ module store_buffer(
 
                 if (funct3 == 3'b000) begin
                     buffer_mem_data[current_block] <= mem_data[7:0]; // SB
+                    funct3s[current_block]<= 3'b000;
                 end else if (funct3 == 3'b001) begin
-                    buffer_mem_data[current_block] <= mem_data[15:0]; // SH  
+                    buffer_mem_data[current_block] <= mem_data[15:0]; // SH
+                    funct3s[current_block]<= 3'b001;  
                 end else if (funct3 == 3'b010) begin
-                    buffer_mem_data[current_block] <= mem_data; // SW  
+                    buffer_mem_data[current_block] <= mem_data; // SW
+                    funct3s[current_block] <= 3'b010;  
                 end
 
 
@@ -92,9 +96,39 @@ module store_buffer(
                         if (buffer_inst_num[i] < inst_num) begin
                             buffer_mem_addr[i] <= 0;  
                             entry_val[i] <= 1'b0;
+                            if(funct3s[i] == 3'b010) begin
+                                if(funct3 == 3'b000) begin
+                                buffer_mem_addr[current_block] <= {buffer_mem_data[i][31:8], mem_data[7:0]}
+                                funct3s[current_block] <= 3'b010;
+                                end
+                                else if(funct3 == 3'b001) begin
+                                buffer_mem_addr[current_block] <= {buffer_mem_data[i][31:16], mem_data[15:0]}
+                                funct3s[current_block] <= 3'b010;
+                                end
+                            end
+                        else if (funct3s[i] == 3'b001) begin
+                             if(funct3 == 3'b000) begin
+                                buffer_mem_addr[current_block] <= {buffer_mem_data[i][31:8], mem_data[7:0]}
+                            end
+                        end
                         end else begin
                             buffer_mem_addr[current_block] <= 0;  
                             entry_val[current_block] <= 1'b0;
+                            if(funct3 == 3'b010) begin
+                                if(funct3s[i] == 3'b000) begin
+                                buffer_mem_addr[i] <= {mem_data[31:8], buffer_mem_data[i][7:0]}
+                                funct3s[i] <= 3'b010;
+                                end
+                                else if(funct3s[i] == 3'b001) begin
+                                buffer_mem_addr[i] <= {mem_data[31:16], buffer_mem_data[i][15:0]}
+                                funct3s[current_block] <= 3'b010;
+                                end
+                            
+                            end else if (funct3 == 3'b001) begin
+                                if(funct3s[i] == 3'b000) begin
+                                    buffer_mem_addr[i] <= {mem_data[31:8], buffer_mem_data[i][7:0]}
+                                end
+                            end
                         end
                     end
                 end
@@ -102,20 +136,29 @@ module store_buffer(
         
      
             end else if (memread) begin
-                load_valid <= 0;
+                load_valid <= 2'b00;
                 load_done_out <= 0;
                 load_phy_out <= load_phy;
                 inst_num_out <= inst_num;
                 for (i = 0; i < SIZE; i = i + 1) begin
                     if (buffer_mem_addr[i] == mem_addr) begin
                         load_data <= buffer_mem_data[i];
-                        load_valid <= 1;
-                        if (funct3 == 3'b000) begin
+                        load_valid <= 2'b11;
+                        if (funct3 == 3'b000) begin 
                             load_data <= {{24{buffer_mem_data[i][7]}}, buffer_mem_data[i][7:0]}; // LB
-                        end else if (funct3 == 3'b001) begin
+                        end else if (funct3 == 3'b001) begin //LH
                             load_data <= {{16{buffer_mem_data[i][15]}}, buffer_mem_data[i][15:0]}; // LH
-                        end else if (funct3 == 3'b010) begin
+                            if(funct3s[i] == 3'b000) begin //하위 8바이트만 맞음
+                                load_valid <= 2'b01;
+                            end
+
+                        end else if (funct3 == 3'b010) begin //하위 16바이트만 맞음
                             load_data <= buffer_mem_data[i]; // LW
+                                if(funct3s[i] == 3'b000) begin
+                                    load_valid <= 2'b01;
+                                end else if(funct3s[i] == 3'b000) begin
+                                    load_valid <= 2'b10;
+                            end
                         end
                     end
                 end
