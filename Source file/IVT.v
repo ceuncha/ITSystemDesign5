fce2ce8302f0257458bda966dfef0475b4052f3f
jjurijang <165987742+jjurijang@users.noreply.github.com>
2024-08-02 23:18:06 +0900

    Update IVT.v

--- Source file/IVT.v
+++ Source file/IVT.v
@@ -1,37 +1,24 @@
 module IVT(
     input clk,
-    input illegal_instruction,
-    input IF_ID_inst_num,
-    input LS,
-    input LS_que_inst_num,
-    input Div_0,
-    input Div_inst_num,
-    input Address,
-    input Address_inst_num,
+    input rob_cause,
 
-    output IVT_inst_num,
+
     output reg [15:0] handler_address,  // 예외 핸들러 주소
-    output reg [3:0] e_cause
+    output reg [1:0] e_cause
 );
 
-always @(posedge clk) begin
-    if (illegal_instruction) begin
+always @(*) begin
+    if (rob_cause== 2'b00) begin
         handler_address <= 16'h02BC; // Illegal Instruction: 700번지
-        IVT_inst_num <= IF_ID_inst_num;
-        e_cause <= 4'b0000;
-    end else if (LS) begin
-        handler_address <= 16'h02E4; // LS: 740번지
-        IVT_inst_num <= LS_que_inst_num;
-        e_cause <=4'b0001;
-    end else if (Div_0) begin
+        e_cause <= 2'b00;
+    end else if (rob_cause == 2'b01) begin
         handler_address <= 16'h030C; // DIV 0: 780번지
-        IVT_inst_num <= Div_inst_num;
-        e_cause <=4'b0010;
-    end else if (Address) begin
+        e_cause <=2'b01;
+    end else if (rob_cause == 2'b10) begin
+        handler_address <= 16'h02E4; // LS: 740번지
+        e_cause <=2'b10;
+    end else if (rob_cause == 2'b11) begin
         handler_address <= 16'h0334; // Address: 820번지
-        IVT_inst_num <= Address_inst_num;
-        e_cause <=4'b0011;
-    end else begin
-        handler_address <= 16'h0000; // Default: 0000번지
+        e_cause <=4'b11;
     end
 end
