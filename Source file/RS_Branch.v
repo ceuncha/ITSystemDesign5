74a6ac07947d6b0b41db55c6ee61c8ef7f528054
jjurijang <165987742+jjurijang@users.noreply.github.com>
2024-08-04 23:39:44 +0900

    Update RS_Branch.v

--- Source file/RS_Branch.v
+++ Source file/RS_Branch.v
@@ -76,23 +76,23 @@ module RS_Branch (                                             //筌뤿굝議�
     integer n;
     integer o;
 
-  (* keep = "true" *)wire operand1_ALU_conflict = ((operand1 == ALU_result_dest)&&ALU_result_valid);
-  (* keep = "true" *)wire operand1_MUL_conflict = ((operand1 == MUL_result_dest)&&MUL_result_valid);
-  (* keep = "true" *)wire operand1_DIV_conflict = ((operand1 == DIV_result_dest)&&DIV_result_valid);
-  (* keep = "true" *)wire operand1_MEM_conflict = ((operand1 == EX_MEM_Physical_Address && EX_MEM_MemRead == 1));
-  (* keep = "true" *)wire operand1_BR_conflict = ((operand1 == BR_Phy)&&BR_Done);
-  (* keep = "true" *)wire operand1_P_conflict = ((operand1 == P_Phy)&&P_Done);
-  (* keep = "true" *)wire operand1_CSR_conflict = ((operand1 == CSR_Phy)&&CSR_Done);
-  (* keep = "true" *)wire operand1_conflict = operand1_ALU_conflict || operand1_MUL_conflict || operand1_DIV_conflict || operand1_MEM_conflict || operand1_BR_conflict || operand1_P_conflict || operand1_CSR_conflict;
+    wire operand1_ALU_conflict = (operand1 == ALU_result_dest);
+    wire operand1_MUL_conflict = (operand1 == MUL_result_dest);
+    wire operand1_DIV_conflict = (operand1 == DIV_result_dest);
+    wire operand1_MEM_conflict = (operand1 == EX_MEM_Physical_Address && EX_MEM_MemRead == 1);
+    wire operand1_BR_conflict = (operand1 == BR_Phy);
+    wire operand1_P_conflict = (operand1 == P_Phy);
+    wire operand1_CSR_conflict = (operand1 == CSR_Phy);
+    wire operand1_conflict = operand1_ALU_conflict || operand1_MUL_conflict || operand1_DIV_conflict || operand1_MEM_conflict || operand1_BR_conflict || operand1_P_conflict || operand1_CSR_conflict;
 
-   (* keep = "true" *)wire operand2_ALU_conflict = ((operand2 == ALU_result_dest)&&ALU_result_valid);
-  (* keep = "true" *)wire operand2_MUL_conflict = ((operand2 == MUL_result_dest)&&MUL_result_valid);
-  (* keep = "true" *)wire operand2_DIV_conflict = ((operand2 == DIV_result_dest)&&DIV_result_valid);
-  (* keep = "true" *)wire operand2_MEM_conflict = (operand2 == EX_MEM_Physical_Address && EX_MEM_MemRead == 1);
-   (* keep = "true" *)wire operand2_BR_conflict = ((operand2 == BR_Phy)&&BR_Done);
-   (* keep = "true" *)wire operand2_P_conflict = ((operand2 == P_Phy)&&P_Done);
-  (* keep = "true" *)wire operand2_CSR_conflict = ((operand2 == CSR_Phy)&&CSR_Done);
-  (* keep = "true" *)wire operand2_conflict = operand2_ALU_conflict || operand2_MUL_conflict || operand2_DIV_conflict || operand2_MEM_conflict || operand2_BR_conflict || operand2_P_conflict || operand2_CSR_conflict;
+    wire operand2_ALU_conflict = (operand2 == ALU_result_dest);
+    wire operand2_MUL_conflict = (operand2 == MUL_result_dest);
+    wire operand2_DIV_conflict = (operand2 == DIV_result_dest);
+    wire operand2_MEM_conflict = (operand2 == EX_MEM_Physical_Address && EX_MEM_MemRead == 1);
+    wire operand2_BR_conflict = (operand2 == BR_Phy);
+    wire operand2_P_conflict = (operand2 == P_Phy);
+    wire operand2_CSR_conflict = (operand2 == CSR_Phy);
+    wire operand2_conflict = operand2_ALU_conflict || operand2_MUL_conflict || operand2_DIV_conflict || operand2_MEM_conflict || operand2_BR_conflict || operand2_P_conflict || operand2_CSR_conflict;
 
     always @(posedge clk) begin    //�뵳�딅��?�뻿?�깈嚥�? �룯�뜃由�?�넅 ?�뻻�녹뮇夷�
         if (reset|exception_sig|mret_sig) begin
@@ -153,7 +153,8 @@ module RS_Branch (                                             //筌뤿굝議�
             Operand1_BR_phy<=0;
             Operand2_BR_phy<=0;
             end
-        end else if (start) begin
+        end else begin
+        if(start) begin
             if ((operand1_conflict == 1'b1) && (operand1_conflict == 1'b0)) begin  // 筌뤿굝議�?堉긷첎? 筌ｌ꼷�벉 ?諭�?堉�?�넅?�뱽?釉�, alu?�벥 野껉퀗�궢?? 筌뤿굝議�?堉�?�벥 operand �눧�눖�봺雅뚯눘�꺖�몴? �뜮袁㏉꺍?釉�?肉� 
                                                     // ?毓�?�쑓?�뵠?�뱜揶�? ?釉�?�뒄?�뻻 ?�땾?六�?鍮먧빳??�뼄.
                 inst_nums[tail] <= RS_BR_inst_num;
@@ -219,7 +220,7 @@ module RS_Branch (                                             //筌뤿굝議�
                 tail <= (tail + 1) % 64;
              end 
              end
-            
+            end
            
             if (ALU_result_valid) begin                 //alu?�벥 野껉퀗�궢揶�? ?諭�?堉�?�넅?�뱽?釉�, 疫꿸퀣��?肉� RS?肉� ?諭�?堉�?�뿳?�쐲 筌뤿굝議�?堉�?諭얏��? �눧�눖�봺雅뚯눘�꺖�몴? �뜮袁㏉꺍?釉�?肉�
                                                         //?釉�?�뒄?釉� 揶쏅�⑸굶?�뱽 ?毓�?�쑓?�뵠?�뱜 ?�뻻�녹뮇??�뼄.
