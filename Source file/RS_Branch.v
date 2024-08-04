c0d649786981d564eade44e726e4b9dc36f74907
jjurijang <165987742+jjurijang@users.noreply.github.com>
2024-08-04 23:42:11 +0900

    Update RS_Branch.v

--- Source file/RS_Branch.v
+++ Source file/RS_Branch.v
@@ -76,23 +76,24 @@ module RS_Branch (                                             //筌뤿굝議�
     integer n;
     integer o;
 
-    wire operand1_ALU_conflict = (operand1 == ALU_result_dest);
-    wire operand1_MUL_conflict = (operand1 == MUL_result_dest);
-    wire operand1_DIV_conflict = (operand1 == DIV_result_dest);
-    wire operand1_MEM_conflict = (operand1 == EX_MEM_Physical_Address && EX_MEM_MemRead == 1);
-    wire operand1_BR_conflict = (operand1 == BR_Phy);
-    wire operand1_P_conflict = (operand1 == P_Phy);
-    wire operand1_CSR_conflict = (operand1 == CSR_Phy);
-    wire operand1_conflict = operand1_ALU_conflict || operand1_MUL_conflict || operand1_DIV_conflict || operand1_MEM_conflict || operand1_BR_conflict || operand1_P_conflict || operand1_CSR_conflict;
+   (* keep = "true" *)wire operand1_ALU_conflict = ((operand1 == ALU_result_dest)&&ALU_result_valid);
+  (* keep = "true" *)wire operand1_MUL_conflict = ((operand1 == MUL_result_dest)&&MUL_result_valid);
+  (* keep = "true" *)wire operand1_DIV_conflict = ((operand1 == DIV_result_dest)&&DIV_result_valid);
+  (* keep = "true" *)wire operand1_MEM_conflict = ((operand1 == EX_MEM_Physical_Address && EX_MEM_MemRead == 1));
+  (* keep = "true" *)wire operand1_BR_conflict = ((operand1 == BR_Phy)&&Branch_result_valid);
+  (* keep = "true" *)wire operand1_P_conflict = ((operand1 == P_Phy)&&P_Done);
+  (* keep = "true" *)wire operand1_CSR_conflict = ((operand1 == CSR_Phy)&&CSR_Done);
+  (* keep = "true" *)wire operand1_conflict = operand1_ALU_conflict || operand1_MUL_conflict || operand1_DIV_conflict || operand1_MEM_conflict || operand1_BR_conflict || operand1_P_conflict || operand1_CSR_conflict;
 
-    wire operand2_ALU_conflict = (operand2 == ALU_result_dest);
-    wire operand2_MUL_conflict = (operand2 == MUL_result_dest);
-    wire operand2_DIV_conflict = (operand2 == DIV_result_dest);
-    wire operand2_MEM_conflict = (operand2 == EX_MEM_Physical_Address && EX_MEM_MemRead == 1);
-    wire operand2_BR_conflict = (operand2 == BR_Phy);
-    wire operand2_P_conflict = (operand2 == P_Phy);
-    wire operand2_CSR_conflict = (operand2 == CSR_Phy);
-    wire operand2_conflict = operand2_ALU_conflict || operand2_MUL_conflict || operand2_DIV_conflict || operand2_MEM_conflict || operand2_BR_conflict || operand2_P_conflict || operand2_CSR_conflict;
+   (* keep = "true" *)wire operand2_ALU_conflict = ((operand2 == ALU_result_dest)&&ALU_result_valid);
+  (* keep = "true" *)wire operand2_MUL_conflict = ((operand2 == MUL_result_dest)&&MUL_result_valid);
+  (* keep = "true" *)wire operand2_DIV_conflict = ((operand2 == DIV_result_dest)&&DIV_result_valid);
+  (* keep = "true" *)wire operand2_MEM_conflict = (operand2 == EX_MEM_Physical_Address && EX_MEM_MemRead == 1);
+   (* keep = "true" *)wire operand2_BR_conflict = ((operand2 == BR_Phy)&&Branch_result_valid);
+   (* keep = "true" *)wire operand2_P_conflict = ((operand2 == P_Phy)&&P_Done);
+  (* keep = "true" *)wire operand2_CSR_conflict = ((operand2 == CSR_Phy)&&CSR_Done);
+  (* keep = "true" *)wire operand2_conflict = operand2_ALU_conflict || operand2_MUL_conflict || operand2_DIV_conflict || operand2_MEM_conflict || operand2_BR_conflict || operand2_P_conflict || operand2_CSR_conflict;
+
 
     always @(posedge clk) begin    //�뵳�딅��?�뻿?�깈嚥�? �룯�뜃由�?�넅 ?�뻻�녹뮇夷�
         if (reset|exception_sig|mret_sig) begin
