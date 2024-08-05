9b48b17c6e79402fe36201bae4014849fbdbb7fc
jjurijang <165987742+jjurijang@users.noreply.github.com>
2024-08-05 13:35:31 +0900

    Update add.v

--- add.v
+++ add.v
@@ -1,8 +1,7 @@
-module add4(
-    input [31:0] in,  // 입력 신호
-    output [31:0] out // 출력 신호
-);
+{memory[0], memory[1], memory[2], memory[3]} <= 32'b00000000001000000001111101110011;
 
-assign out = in + 4;
+{memory[4], memory[5], memory[6], memory[7]} <= 32'b00000000000100000001111011110011;
 
-endmodule
+{memory[8], memory[9], memory[10], memory[11]} <= 32'b00110101110000000000111001100111;
+
+{memory[12], memory[13], memory[14], memory[15]} <= 32'b00110000001000000000000001110011;
