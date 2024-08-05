1c89a499ebac1fc45dd5a9345908c26f43546737
jjurijang <165987742+jjurijang@users.noreply.github.com>
2024-08-05 17:24:36 +0900

    Update Pipeline_Reg.v

--- Source file/Pipeline_Reg.v
+++ Source file/Pipeline_Reg.v
@@ -7,6 +7,7 @@ module ifid_pipeline_register (
     input Predict_Result,
     input taken,
     input hit,
+    input exception_sig,
     output reg IF_ID_taken,
     output reg [31:0] IF_ID_instOut,  
     output reg [31:0] IF_ID_inst_num,
@@ -14,26 +15,25 @@ module ifid_pipeline_register (
     output reg ROB_Flush,
     output reg IF_ID_hit
 );
-    
-    always @(posedge clk or posedge reset) begin
+    always @(posedge clk) begin
         if (reset) begin
-            // ?逾�?�봾?? ??六�??源덃뤆?? ??�꽎?�땻???�꼨?�뵳釉앹춺? ?猷�?�쐝�뵳???�꼨
+            // ?�억옙?占쎈늅?? ??筌묕옙??繹먮뛽琉�?? ??占쎄퐥?占쎈빝???占쎄섀?占쎈뎨�뇡�빘異�? ?�뙴占�?占쎌맃占쎈뎨???占쎄섀
             IF_ID_instOut <= 32'b0;
             IF_ID_inst_num <= 32'b0;
             IF_ID_PC <= 32'b0;
             ROB_Flush <= 1'b0;
             IF_ID_taken<= 1'b0;
             IF_ID_hit <= 1'b0;
-
-        end else if (Predict_Result) begin
+        end else if (exception_sig) begin
             IF_ID_instOut <= 32'b0;
             IF_ID_inst_num <= 32'b0;
             IF_ID_PC <= 32'b0;
-            ROB_Flush <= 1'b1;
+            ROB_Flush <= 1'b0;
             IF_ID_taken<= 1'b0;
             IF_ID_hit <= 1'b0;
+            ROB_Flush <= 1'b1;
         end else begin
-            // ?��???�몠??六삥뤆?? ?�뇡???鍮�??? ??裕�??苑�?�뙴? ?�뇡???鍮� ?�뇡? ??�젧?疫�? ?�뙴???�굚
+            // ?占쏙옙???占쎈첓??筌묒궏琉�?? ?占쎈눀???�뜮占�??? ??獒뺧옙??�땻占�?占쎈쇀? ?占쎈눀???�뜮占� ?占쎈눀? ??占쎌젳?�뼨占�? ?占쎈쇀???占쎄탾
             IF_ID_instOut <= instOut;
             IF_ID_inst_num <= inst_num;
             IF_ID_PC <= PC;
