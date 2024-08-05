334b35b09266b27eed84151ad3fda6695b2ed1c8
jjurijang <165987742+jjurijang@users.noreply.github.com>
2024-08-05 15:43:20 +0900

    Update IVT.v

--- Source file/IVT.v
+++ Source file/IVT.v
@@ -1,19 +1,17 @@
 module IVT(
     input [1:0] rob_cause,
-    output reg [31:0] handler_address  // 예외 핸들러 주소
+    output reg [31:0] handler_address  // ?삁?쇅 ?빖?뱾?윭 二쇱냼
 );
 
 always @(*) begin
     if (rob_cause== 2'b00) begin
-        handler_address <= 32'h02BC; // Illegal Instruction: 700번지
+        handler_address <= 32'h02BC; // Illegal Instruction: 700踰덉?
     end else if (rob_cause == 2'b01) begin
-        handler_address <= 32'h030C; // DIV 0: 780번지
+        handler_address <= 32'h030C; // DIV 0: 780踰덉?
     end else if (rob_cause == 2'b10) begin
-        handler_address <= 32'h02E4; // LS: 740번지
+        handler_address <= 32'h02E4; // LS: 740踰덉?
     end else if (rob_cause == 2'b11) begin
-        handler_address <= 32'h0334; // Address: 820번지
-    end else begin
-        handler_address <=32'h0000;
+        handler_address <= 32'h0334; // Address: 820踰덉?
     end
     end
 endmodule
