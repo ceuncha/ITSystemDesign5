b647867099b5e6149df47957117c362dbbf1c604
jjurijang <165987742+jjurijang@users.noreply.github.com>
2024-08-08 00:39:13 +0900

    Update DataMemory.v

--- Source file/DataMemory.v
+++ Source file/DataMemory.v
@@ -1,187 +1,71 @@
-module store_buffer(
-    input clk,
-    input reset,
-    input exception,
-    input memwrite,
-    input memread,
-    input mret_sig,
-
-    input [2:0] funct3,
-    input [7:0] load_phy,
-    input [31:0] inst_num,
-    input [31:0] mem_addr,
-    input [31:0] mem_data,
-
-
-    output reg [31:0] load_data,
-    output reg [7:0] load_phy_out,
-    output reg [31:0] inst_num_out,
-    output reg [2:0] load_valid,
-
-    output reg load_done_out,
-    output reg exception_flag,
-    output reg [31:0] store_address_out,
-
-    input memwrite_rob,
-    input [31:0] mem_addr_rob,
-    input [31:0] inst_num_rob
+module DataMemory(
+    input wire ROB_MemWrite,
+    input wire [31:0] ROB_memadress,
+    input wire [2:0] ROB_funct3,
+    input wire clk,
+    input wire reset,
+    input wire [2:0] func3_LS,
+    input wire [31:0] out_value,
+    input wire [31:0] LS_result,
+    input wire LS_MemRead,
+    
+    input wire ROB_MemRead,  // 08.07 new update
+    output reg exception_datamem,
+    output reg [31:0] Data_memory_out
 );
-    parameter SIZE = 32;
-    
-    reg [31:0] buffer_inst_num [0:SIZE-1];
-    reg [31:0] buffer_mem_addr [0:SIZE-1];
-    reg [31:0] buffer_mem_data [0:SIZE-1];
-    reg [2:0] funct3s [0:SIZE-1];
-
-    reg [SIZE-1:0] entry_val;
-    reg [4:0] current_block;
-    reg [4:0] next_block;
-    integer i;
-    
-    // Reset logic
-    always @(posedge clk) begin
-
-        if (reset || exception || mret_sig) begin
-            for (i = 0; i < SIZE; i = i + 1) begin
-                buffer_inst_num[i] <= 0;
-                buffer_mem_addr[i] <= 0;
-                buffer_mem_data[i] <= 0;
-                entry_val[i] <= 0;
-                funct3s [i] <= 0;
-            end
-            current_block <= 0;
-            next_block <= 1;
-            load_data <= 0;
-            load_valid <= 0;
-            load_phy_out <= 0;
-            load_done_out <= 0;
-            store_address_out <= 0;
-            exception_flag <= 0;
-
-        end else begin
-            load_done_out <= 0;
-            load_phy_out <= load_phy;
-            inst_num_out <= inst_num;
-            store_address_out <= 0;
-            if(memwrite_rob) begin
-                for (i = 0; i < SIZE; i = i + 1) begin
-                    if(buffer_inst_num[i]==inst_num_rob) begin
-                            entry_val[i] <= 0;
-                            buffer_inst_num[i] <= 0;
-                            buffer_mem_addr[i] <= 0;
-                    end
-                end
-            end
-            
-
-
-            
-            if (memwrite) begin
-                load_done_out <= 1;
-                for (i = SIZE-1; i >= 0; i = i - 1) begin
-                    if(!entry_val[i] && (i != current_block)&&(i != next_block)) begin
-                            next_block <= i;
-                    end
-                end
-
-                buffer_mem_addr[current_block] <= mem_addr;  
-                buffer_inst_num[current_block] <= inst_num; 
-                entry_val[current_block] <= 1'b1;
-                inst_num_out <= inst_num;
-                load_phy_out <= 8'd160;
-                store_address_out <= mem_addr;
-
-                if (funct3 == 3'b000) begin
-                    buffer_mem_data[current_block] <= mem_data[7:0]; // SB
-                    funct3s[current_block]<= 3'b000;
-                end else if (funct3 == 3'b001) begin
-                    buffer_mem_data[current_block] <= mem_data[15:0]; // SH
-                    funct3s[current_block]<= 3'b001;  
-                end else if (funct3 == 3'b010) begin
-                    buffer_mem_data[current_block] <= mem_data; // SW
-                    funct3s[current_block] <= 3'b010;  
-                end
-
-
-
-            // Check for existing entry with the same address
-                for (i = 0; i < SIZE; i = i + 1) begin
-                    if (buffer_mem_addr[i] == mem_addr) begin
-                        if (buffer_inst_num[i] < inst_num) begin
-                            buffer_mem_addr[i] <= 0;  
-                            entry_val[i] <= 1'b0;
-                            if(funct3s[i] == 3'b010) begin
-                                if(funct3 == 3'b000) begin
-                                buffer_mem_data[current_block] <= {buffer_mem_data[i][31:8], mem_data[7:0]};
-                                funct3s[current_block] <= 3'b010;
-                                end
-                                else if(funct3 == 3'b001) begin
-                                buffer_mem_data[current_block] <= {buffer_mem_data[i][31:16], mem_data[15:0]};
-                                funct3s[current_block] <= 3'b010;
-                                end
-                            end
-                        else if (funct3s[i] == 3'b001) begin
-                             if(funct3 == 3'b000) begin
-                                buffer_mem_data[current_block] <= {buffer_mem_data[i][31:8], mem_data[7:0]};
-                            end
-                        end
-                        end else begin
-                            buffer_mem_addr[current_block] <= 0;  
-                            entry_val[current_block] <= 1'b0;
-                            if(funct3 == 3'b010) begin
-                                if(funct3s[i] == 3'b000) begin
-                                buffer_mem_data[i] <= {mem_data[31:8], buffer_mem_data[i][7:0]};
-                                funct3s[i] <= 3'b010;
-                                end
-                                else if(funct3s[i] == 3'b001) begin
-                                buffer_mem_data[i] <= {mem_data[31:16], buffer_mem_data[i][15:0]};
-                                funct3s[current_block] <= 3'b010;
-                                end
-                            
-                            end else if (funct3 == 3'b001) begin
-                                if(funct3s[i] == 3'b000) begin
-                                    buffer_mem_data[i] <= {mem_data[31:8], buffer_mem_data[i][7:0]};
-                                end
-                            end
-                        end
-                    end
-                end
-            current_block <= next_block;
-        
-     
-            end else if (memread) begin
-                load_valid <= 3'b000;
-                load_done_out <= 1'b1;
-                store_address_out <= mem_addr;
-                for (i = 0; i < SIZE; i = i + 1) begin
-                    if (buffer_mem_addr[i] == mem_addr) begin
-
-                        load_data <= buffer_mem_data[i];
-                        load_valid <= 3'b111;
-                        if(buffer_inst_num[i] > inst_num) begin
-                            load_valid <= 3'b000;
-                        end else if (funct3 == 3'b000) begin 
-                            load_data <= {{24{buffer_mem_data[i][7]}}, buffer_mem_data[i][7:0]}; // LB
-                        end else if (funct3 == 3'b001) begin //LH
-                            load_data <= {{16{buffer_mem_data[i][15]}}, buffer_mem_data[i][15:0]}; // LH
-                            if(funct3s[i] == 3'b000) begin //하위 8바이트만 맞음
-                                load_valid <= 3'b001;
-                            end
-
-                        end else if (funct3 == 3'b010) begin //하위 16바이트만 맞음
-                            load_data <= buffer_mem_data[i]; // LW
-                                if(funct3s[i] == 3'b000) begin
-                                    load_valid <= 3'b010;
-                                end else if(funct3s[i] == 3'b001) begin
-                                    load_valid <= 3'b011;
-                            end
-                        end
-                    end
-                end
-            end
-            
-        end   
-
+reg [31:0]load_data;   //08.07 new update
+integer i;
+reg [31:0] memory [0:2047];
+    always @(posedge clk) begin                       //由ъ뀑?떆?뿉 媛? 踰덉??닔?쓽 ?뜲?씠?꽣?뒗 踰덉??닔+3?쑝濡? ?븳?떎,
+   if (reset) begin
+       for (i = 0; i < 2047; i = i + 1) begin
+            memory[i] <= i+3;
+        end
+        exception_datamem <= 0;
     end
+    if (ROB_MemWrite) begin
+        case (ROB_funct3)
+            3'b000: memory[ROB_memadress][7:0] <= out_value[7:0]; // SB
+            3'b001: memory[ROB_memadress][15:0] <= out_value[15:0]; // SH
+            3'b010: memory[ROB_memadress] <= out_value; // SW
+            default: ; // No operation (NOP) for other funct3 values, explicitly defined
+        endcase
+    end
+    // Default value for RData, ensures it is always assigned
+    Data_memory_out = 32'd0; // if MemRead is false
+    if (LS_MemRead) begin
+        case (func3_LS)
+            3'b000: Data_memory_out = {{24{memory[LS_result][31]}}, memory[LS_result][7:0]}; // LB
+            3'b001: Data_memory_out = {{16{memory[LS_result][31]}}, memory[LS_result][15:0]}; // LH
+            3'b010: Data_memory_out = memory[LS_result]; // LW
+            3'b100: Data_memory_out = {{24{1'b0}}, memory[LS_result][7:0]}; // LBU
+            3'b101: Data_memory_out = {{16{1'b0}}, memory[LS_result][15:0]}; // LHU
+            default: Data_memory_out = 32'd0; // Default value assignment to handle cases when MemRead is false
+        endcase
+    end
+  end
+
+
+   always @(*) begin
+
+    
+    if (ROB_MemRead ) begin  // 08.07 new update
+    case (ROB_funct3)
+            3'b000: load_data = {{24{memory[ROB_memadress][31]}}, memory[ROB_memadress][7:0]}; // LB
+            3'b001: load_data = {{16{memory[ROB_memadress][31]}}, memory[ROB_memadress][15:0]}; // LH
+            3'b010: load_data = memory[ROB_memadress]; // LW
+            3'b100: load_data = {{24{1'b0}}, memory[ROB_memadress][7:0]}; // LBU
+            3'b101: load_data = {{16{1'b0}}, memory[ROB_memadress][15:0]}; // LHU
+            default: load_data = 32'd0; // Default value assignment to handle cases when MemRead is false
+        endcase
+        if (load_data != out_value) begin
+        exception_datamem = 1;
+        end else begin
+        exception_datamem=0;
+        end
+        end else begin
+        exception_datamem=0;
+        end
+  end
 
 endmodule
