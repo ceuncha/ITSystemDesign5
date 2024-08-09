0fa4ffdab46d7f60256fa8c8cfcbcd6bc1bb34a5
hominki <165993704+hominki@users.noreply.github.com>
2024-08-09 15:19:28 +0900

    Update mem_buffer.v

--- Source file/mem_buffer.v
+++ Source file/mem_buffer.v
@@ -1,6 +1,5 @@
 module mem_buffer(
     input wire clk,
-    input wire reset,
     input wire LS_MemWrite,
     input wire LS_MemRead,
 
@@ -21,26 +20,13 @@ module mem_buffer(
     output reg [31:0] LS_inst_num_buff,  
     output reg [31:0] Operand2_LS_buff,  //Write_data
 
+    output reg memwrite_rob,    
     output reg [31:0] mem_addr_rob_buff, //mem addr from rob
     output reg [31:0] inst_num_rob_buff //inst num from rob
 );
 
 
 always @(posedge clk) begin
-    if(reset) begin
-        
-    LS_MemWrite_buff <= 0;
-    LS_MemRead_buff <= 0;
-    func3_LS_buff <= 0;
-    LS_Result_buff <= 0;
-    Load_Phy_buff <= 0;
-    LS_inst_num_buff <= 0;
-    Operand2_LS_buff <= 0;
-    mem_addr_rob_buff <= 0;
-    inst_num_rob_buff <= 0;
-        
-    end else begin
-    
     LS_MemWrite_buff <= LS_MemWrite;
     LS_MemRead_buff <= LS_MemRead;
     func3_LS_buff <= func3_LS;
@@ -50,7 +36,7 @@ always @(posedge clk) begin
     Operand2_LS_buff <= Operand2_LS;
     mem_addr_rob_buff <= mem_addr_rob;
     inst_num_rob_buff <= inst_num_rob;
-        
-    end
+    memwrite_rob_buff <= memwrite_rob;
+end
 
 endmodule
