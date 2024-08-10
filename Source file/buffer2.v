e6eab36e9b78eea042e5e0e851b7522ccab78bf0
jjurijang <165987742+jjurijang@users.noreply.github.com>
2024-08-10 18:53:14 +0900

    Update buffer2.v

--- Source file/buffer2.v
+++ Source file/buffer2.v
@@ -8,8 +8,7 @@ module buffer2(
     input wire [7:0] BR_Phy,  
     input wire [31:0] RS_BR_inst_num_output, 
     input wire [31:0] immediate_BR,
-    input wire [7:0] Operand1_BR_phy,
-    input wire [7:0] Operand2_BR_phy,
+
     input wire [31:0] Operand2_BR,
     input wire [2:0] RS_BR_funct3,
     input wire negative,
@@ -17,6 +16,7 @@ module buffer2(
     input wire overflow,
     input wire carry, 
     input wire PC_Return,
+    
     output reg b_RS_BR_Branch,
     output reg b_RS_BR_Jump,
     output reg b_RS_BR_taken,
@@ -24,8 +24,7 @@ module buffer2(
   output reg [7:0] b_BR_Phy,  
   output reg [31:0] b_RS_BR_inst_num_output, 
   output reg [31:0] b_immediate_BR,
-  output reg [7:0] b_Operand1_BR_phy,
-  output reg [7:0] b_Operand2_BR_phy,
+
     output reg [2:0] b_RS_BR_funct3,
     output reg [31:0] b_Operand2_BR,
     output reg b_negative,
@@ -47,8 +46,7 @@ always @(posedge clk) begin
     b_RS_BR_inst_num_output <= 0;
     b_immediate_BR <= 0;
     b_PC_BR <= 0;
-    b_Operand1_BR_phy <= 0;
-    b_Operand2_BR_phy <= 0;
+
     b_RS_BR_funct3 <= 0;
     b_negative <= 0;
     b_zero <= 0;
@@ -65,8 +63,7 @@ always @(posedge clk) begin
     b_RS_BR_inst_num_output <= RS_BR_inst_num_output;
     b_immediate_BR <= immediate_BR;
     b_PC_BR <= PC_BR;
-    b_Operand1_BR_phy <= Operand1_BR_phy;
-    b_Operand2_BR_phy <= Operand2_BR_phy;
+
     b_RS_BR_funct3 <= RS_BR_funct3;
     b_negative <= negative;
     b_zero <= zero;
