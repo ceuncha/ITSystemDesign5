c3117f6c2266241c8d7953e91a7ec9542154eb32
hominki <165993704+hominki@users.noreply.github.com>
2024-08-17 16:41:08 +0900

    Update DataMemory.v

--- Source file/DataMemory.v
+++ Source file/DataMemory.v
@@ -25,16 +25,25 @@ reg [7:0] memory [0:2047];
         end else begin
             if (ROB_MemWrite) begin
                 case (ROB_funct3)
-                    3'b000: memory[ROB_memadress] <= out_value[7:0]; // SB
-                    3'b001: memory[ROB_memadress]<= out_value[7:0]; // SH
-                            memory[ROB_memadress+1]<= out_value[15:8]; 
-                    3'b010: memory[ROB_memadress] <= out_value[7:0]; // SW
-                            memory[ROB_memadress+1] <= out_value[15:8]; // SW
-                            memory[ROB_memadress+2] <= out_value[23:16]; // SW
-                            memory[ROB_memadress+3] <= out_value[31:24]; // SW
+                    3'b000: 
+                        memory[ROB_memadress] <= out_value[7:0]; // SB
+                    
+                    3'b001: begin
+                        memory[ROB_memadress] <= out_value[7:0]; // SH
+                        memory[ROB_memadress + 1] <= out_value[15:8]; 
+                    end
+                    
+                    3'b010: begin
+                        memory[ROB_memadress] <= out_value[7:0]; // SW
+                        memory[ROB_memadress + 1] <= out_value[15:8];
+                        memory[ROB_memadress + 2] <= out_value[23:16];
+                        memory[ROB_memadress + 3] <= out_value[31:24];
+                    end
+                    
                     default: ; // No operation (NOP) for other funct3 values, explicitly defined
                 endcase
             end
+            
             // Default value for RData, ensures it is always assigned
             Data_memory_out = 32'd0; // if MemRead is false
             if (LS_MemRead) begin
