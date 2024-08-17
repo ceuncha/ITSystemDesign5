95f4cca083a18030ce7d6ffebd0f67facadeb9c0
jjurijang <165987742+jjurijang@users.noreply.github.com>
2024-08-17 23:37:15 +0900

    Update add.v

--- add.v
+++ add.v
@@ -1,69 +1,141 @@
-module Instruction_memory(
-
-    input wire [31:0] pc,
-
+module DataMemory(
+    input wire ROB_MemWrite,
+    input wire [31:0] ROB_memadress,
+    input wire [2:0] ROB_funct3,
+    input wire clk,
     input wire reset,
-    output reg [31:0] instOut
-
+    input wire [2:0] func3_LS,
+    input wire [31:0] out_value,
+    input wire [31:0] LS_result,
+    input wire LS_MemRead,
+    
+    input wire ROB_MemRead,  // 08.07 new update
+    output reg exception_datamem,
+    output reg [31:0] Data_memory_out
 );
-  reg [7:0] memory [0:1023]; // 1KB memory
+reg [31:0]load_data;   //08.07 new update
 integer i;
-
-always @(posedge reset) begin
-
-        for (i = 0; i < 1024; i = i + 1) begin
-
-            memory[i] <= 32'b0;
-
+reg [31:0] memory [0:4095];
+    always @(posedge clk) begin                       
+   if (reset) begin
+       for (i = 0; i < 2048; i = i + 1) begin
+           memory[i] <= i+3;
         end
 
-// ADDI x2, x0, 12
-{memory[0], memory[1], memory[2], memory[3]} <= 32'b000000001100_00000_000_00010_0010011;
-// ADDI x3, x0, 1
-{memory[4], memory[5], memory[6], memory[7]} <= 32'b000000000001_00000_000_00011_0010011;
-// MUL x1, x1, x2
-{memory[8], memory[9], memory[10], memory[11]} <= 32'b0000001_00010_00001_000_00001_0110011;
-// DIV x1, x1, x3
-{memory[12], memory[13], memory[14], memory[15]} <= 32'b00000010000000001100000010110011;
-// ADDI x2, x2, -1
-{memory[16], memory[17], memory[18], memory[19]} <= 32'b111111111111_00010_000_00010_0010011;
-// ADDI x3, x3, 1
-{memory[20], memory[21], memory[22], memory[23]} <= 32'b000000000001_00011_000_00011_0010011;
-//addi x10,x7,2000
-{memory[24], memory[25], memory[26], memory[27]} <= 32'b01111101000000111000010100010011;
-// lw x5, 2000(x10)
-{memory[28], memory[29], memory[30], memory[31]} <= 32'b01111101000001010010001010000011;
+    end
+    if (ROB_MemWrite) begin
+        case (ROB_funct3)
+            3'b000: memory[ROB_memadress][7:0] <= out_value[7:0]; // SB
+            3'b001: memory[ROB_memadress][15:0] <= out_value[15:0]; // SH
+            3'b010: memory[ROB_memadress] <= out_value; // SW
+            default: ; // No operation (NOP) for other funct3 values, explicitly defined
+        endcase
+    end
+    // Default value for RData, ensures it is always assigned
+    Data_memory_out = 32'd0; // if MemRead is false
+    if (LS_MemRead) begin
+        case (func3_LS)
+            3'b000: Data_memory_out = {{24{memory[LS_result][31]}}, memory[LS_result][7:0]}; // LB
+            3'b001: Data_memory_out = {{16{memory[LS_result][31]}}, memory[LS_result][15:0]}; // LH
+            3'b010: Data_memory_out = memory[LS_result]; // LW
+            3'b100: Data_memory_out = {{24{1'b0}}, memory[LS_result][7:0]}; // LBU
+            3'b101: Data_memory_out = {{16{1'b0}}, memory[LS_result][15:0]}; // LHU
+            default: Data_memory_out = 32'd0; // Default value assignment to handle cases when MemRead is false
+        endcase
+    end
+  end
 
-{memory[700], memory[701], memory[702], memory[703]} <= 32'b00000000001000000001111101110011;
 
-{memory[704], memory[705], memory[706], memory[707]} <= 32'b00000000000100000001111011110011;
+   always @(*) begin
+    exception_datamem=0;
+    
+    if (ROB_MemRead ) begin  // 08.07 new update
+    case (ROB_funct3)
+            3'b000: load_data = {{24{memory[ROB_memadress][31]}}, memory[ROB_memadress][7:0]}; // LB
+            3'b001: load_data = {{16{memory[ROB_memadress][31]}}, memory[ROB_memadress][15:0]}; // LH
+            3'b010: load_data = memory[ROB_memadress]; // LW
+            3'b100: load_data = {{24{1'b0}}, memory[ROB_memadress][7:0]}; // LBU
+            3'b101: load_data = {{16{1'b0}}, memory[ROB_memadress][15:0]}; // LHU
+            default: load_data = 32'd0; // Default value assignment to handle cases when MemRead is false
+        endcase
+        if (load_data != out_value) begin
+        exception_datamem = 1;
+        end else begin
+        exception_datamem=0;
+        end
+        end else begin
+        exception_datamem=0;
+        end
+  end
 
-{memory[708], memory[709], memory[710], memory[711]} <= 32'b00110101110000000000111001100111;
-
-{memory[740], memory[741], memory[742], memory[743]} <= 32'b00000000001000000001111101110011;
-
-{memory[744], memory[745], memory[746], memory[747]} <= 32'b00000000000100000001111011110011;
-
-{memory[748], memory[749], memory[750], memory[751]} <= 32'b00110101110000000000111001100111;
-
-{memory[780], memory[781], memory[782], memory[783]} <= 32'b00000000001000000001111101110011;
-
-{memory[784], memory[785], memory[786], memory[787]} <= 32'b00000000000100000001111011110011;
-
-{memory[788], memory[789], memory[790], memory[791]} <= 32'b00110000001000000000000001110011;
-
-{memory[820], memory[821], memory[822], memory[823]} <= 32'b00000000001000000001111101110011;
-
-{memory[824], memory[825], memory[826], memory[827]} <= 32'b00000000000100000001111011110011;
-
-{memory[828], memory[829], memory[830], memory[831]} <= 32'b00110101110000000000111001100111;
-
+endmodulemodule DataMemory(
+    input wire ROB_MemWrite,
+    input wire [31:0] ROB_memadress,
+    input wire [2:0] ROB_funct3,
+    input wire clk,
+    input wire reset,
+    input wire [2:0] func3_LS,
+    input wire [31:0] out_value,
+    input wire [31:0] LS_result,
+    input wire LS_MemRead,
+    
+    input wire ROB_MemRead,  // 08.07 new update
+    output reg exception_datamem,
+    output reg [31:0] Data_memory_out
+);
+reg [31:0]load_data;   //08.07 new update
+integer i;
+reg [31:0] memory [0:4095];
+    always @(posedge clk) begin                       
+   if (reset) begin
+       for (i = 0; i < 2048; i = i + 1) begin
+           memory[i] <= i+3;
+        end
 
     end
-    always @ (*) begin
-
-        instOut <= {memory[pc], memory[pc+1], memory[pc+2], memory[pc+3]};
-
+    if (ROB_MemWrite) begin
+        case (ROB_funct3)
+            3'b000: memory[ROB_memadress][7:0] <= out_value[7:0]; // SB
+            3'b001: memory[ROB_memadress][15:0] <= out_value[15:0]; // SH
+            3'b010: memory[ROB_memadress] <= out_value; // SW
+            default: ; // No operation (NOP) for other funct3 values, explicitly defined
+        endcase
     end
+    // Default value for RData, ensures it is always assigned
+    Data_memory_out = 32'd0; // if MemRead is false
+    if (LS_MemRead) begin
+        case (func3_LS)
+            3'b000: Data_memory_out = {{24{memory[LS_result][31]}}, memory[LS_result][7:0]}; // LB
+            3'b001: Data_memory_out = {{16{memory[LS_result][31]}}, memory[LS_result][15:0]}; // LH
+            3'b010: Data_memory_out = memory[LS_result]; // LW
+            3'b100: Data_memory_out = {{24{1'b0}}, memory[LS_result][7:0]}; // LBU
+            3'b101: Data_memory_out = {{16{1'b0}}, memory[LS_result][15:0]}; // LHU
+            default: Data_memory_out = 32'd0; // Default value assignment to handle cases when MemRead is false
+        endcase
+    end
+  end
+
+
+   always @(*) begin
+    exception_datamem=0;
+    
+    if (ROB_MemRead ) begin  // 08.07 new update
+    case (ROB_funct3)
+            3'b000: load_data = {{24{memory[ROB_memadress][31]}}, memory[ROB_memadress][7:0]}; // LB
+            3'b001: load_data = {{16{memory[ROB_memadress][31]}}, memory[ROB_memadress][15:0]}; // LH
+            3'b010: load_data = memory[ROB_memadress]; // LW
+            3'b100: load_data = {{24{1'b0}}, memory[ROB_memadress][7:0]}; // LBU
+            3'b101: load_data = {{16{1'b0}}, memory[ROB_memadress][15:0]}; // LHU
+            default: load_data = 32'd0; // Default value assignment to handle cases when MemRead is false
+        endcase
+        if (load_data != out_value) begin
+        exception_datamem = 1;
+        end else begin
+        exception_datamem=0;
+        end
+        end else begin
+        exception_datamem=0;
+        end
+  end
 
 endmodule
