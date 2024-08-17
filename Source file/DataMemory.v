261eb6178739d3131436c1ec4a171eb67f0af3bc
hominki <165993704+hominki@users.noreply.github.com>
2024-08-17 15:55:57 +0900

    Update DataMemory.v

--- Source file/DataMemory.v
+++ Source file/DataMemory.v
@@ -1,4 +1,4 @@
-module DataMemory(
+module DataMemory(      // 08.17 new update
     input wire ROB_MemWrite,
     input wire [31:0] ROB_memadress,
     input wire [2:0] ROB_funct3,
@@ -15,7 +15,7 @@ module DataMemory(
 );
 reg [31:0]load_data;   //08.07 new update
 integer i;
-reg [31:0] memory [0:2047];
+reg [7:0] memory [0:2047];
     always @(posedge clk) begin                       
    if (reset) begin
        for (i = 0; i < 2048; i = i + 1) begin
@@ -25,9 +25,13 @@ reg [31:0] memory [0:2047];
     end
     if (ROB_MemWrite) begin
         case (ROB_funct3)
-            3'b000: memory[ROB_memadress][7:0] <= out_value[7:0]; // SB
-            3'b001: memory[ROB_memadress][15:0] <= out_value[15:0]; // SH
-            3'b010: memory[ROB_memadress] <= out_value; // SW
+            3'b000: memory[ROB_memadress] <= out_value[7:0]; // SB
+            3'b001: memory[ROB_memadress]<= out_value[7:0]; // SH
+                    memory[ROB_memadress+1]<= out_value[15:8]; 
+            3'b010: memory[ROB_memadress] <= out_value[7:0]; // SW
+                    memory[ROB_memadress+1] <= out_value[15:8]; // SW
+                    memory[ROB_memadress+2] <= out_value[23:16]; // SW
+                    memory[ROB_memadress+3] <= out_value[31:24]; // SW
             default: ; // No operation (NOP) for other funct3 values, explicitly defined
         endcase
     end
@@ -35,11 +39,11 @@ reg [31:0] memory [0:2047];
     Data_memory_out = 32'd0; // if MemRead is false
     if (LS_MemRead) begin
         case (func3_LS)
-            3'b000: Data_memory_out = {{24{memory[LS_result][31]}}, memory[LS_result][7:0]}; // LB
-            3'b001: Data_memory_out = {{16{memory[LS_result][31]}}, memory[LS_result][15:0]}; // LH
-            3'b010: Data_memory_out = memory[LS_result]; // LW
-            3'b100: Data_memory_out = {{24{1'b0}}, memory[LS_result][7:0]}; // LBU
-            3'b101: Data_memory_out = {{16{1'b0}}, memory[LS_result][15:0]}; // LHU
+            3'b000: Data_memory_out = {{24{memory[LS_result][31]}}, memory[LS_result]}; // LB
+            3'b001: Data_memory_out = {{16{memory[LS_result][31]}}, memory[LS_result+1], memory[LS_result]}; // LH
+            3'b010: Data_memory_out = {memory[LS_result+3], memory[LS_result+2], memory[LS_result+1], memory[LS_result]}; // LW
+            3'b100: Data_memory_out = {{24{1'b0}}, memory[LS_result]}; // LBU
+            3'b101: Data_memory_out = {{16{1'b0}}, memory[LS_result+1], memory[LS_result]}; // LHU
             default: Data_memory_out = 32'd0; // Default value assignment to handle cases when MemRead is false
         endcase
     end
@@ -49,13 +53,13 @@ reg [31:0] memory [0:2047];
    always @(*) begin
     exception_datamem=0;
     
-    if (ROB_MemRead ) begin  // 08.07 new update
+    if (ROB_MemRead ) begin  
     case (ROB_funct3)
-            3'b000: load_data = {{24{memory[ROB_memadress][31]}}, memory[ROB_memadress][7:0]}; // LB
-            3'b001: load_data = {{16{memory[ROB_memadress][31]}}, memory[ROB_memadress][15:0]}; // LH
-            3'b010: load_data = memory[ROB_memadress]; // LW
-            3'b100: load_data = {{24{1'b0}}, memory[ROB_memadress][7:0]}; // LBU
-            3'b101: load_data = {{16{1'b0}}, memory[ROB_memadress][15:0]}; // LHU
+            3'b000: load_data = {{24{memory[ROB_memadress][31]}}, memory[ROB_memadress]}; // LB
+            3'b001: load_data = {{16{memory[ROB_memadress][31]}}, memory[ROB_memadress+1], memory[ROB_memadress]}; // LH
+            3'b010: load_data = {memory[ROB_memadress+3], memory[ROB_memadress+2], memory[ROB_memadress+1], memory[ROB_memadress]}; // LW
+            3'b100: load_data = {{24{1'b0}}, memory[ROB_memadress]}; // LBU
+            3'b101: load_data = {{16{1'b0}}, memory[ROB_memadress+1], memory[ROB_memadress]}; // LHU
             default: load_data = 32'd0; // Default value assignment to handle cases when MemRead is false
         endcase
         if (load_data != out_value) begin
