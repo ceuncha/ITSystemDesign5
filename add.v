70c31ed3d2e06c07a413c82d165a61dcff812b51
jjurijang <165987742+jjurijang@users.noreply.github.com>
2024-08-18 00:09:58 +0900

    Update add.v

--- add.v
+++ add.v
@@ -1,141 +1,42 @@
-module DataMemory(
-    input wire ROB_MemWrite,
-    input wire [31:0] ROB_memadress,
-    input wire [2:0] ROB_funct3,
-    input wire clk,
-    input wire reset,
-    input wire [2:0] func3_LS,
-    input wire [31:0] out_value,
-    input wire [31:0] LS_result,
-    input wire LS_MemRead,
-    
-    input wire ROB_MemRead,  // 08.07 new update
-    output reg exception_datamem,
-    output reg [31:0] Data_memory_out
-);
-reg [31:0]load_data;   //08.07 new update
-integer i;
-reg [31:0] memory [0:4095];
-    always @(posedge clk) begin                       
-   if (reset) begin
-       for (i = 0; i < 2048; i = i + 1) begin
-           memory[i] <= i+3;
-        end
+@00000000
+1f 40 02 93 // addi x5,x0,500
+00 44 04 13 // addi x8,x8,4
+00 b2 86 b3 // add x13,x5,x11
+00 44 00 b3 //add x1,x8,x4
+00 40 07 93 //addi x15, x0, 4
+00 80 07 93 //addi x15, x0, 8
+01 00 07 93 // addi x15, x0, 16
+40 42 87 33 // sub x14, x5, x4
+00 45 c5 b3 // xor x11,x11,x4
+00 44 06 33 // add x12,x8,x4
+00 d4 20 a3 // sw x13, 1(x8)
+00 14 21 03 // lw x2, 1(x8)
+00 41 01 b3 // add x3,x2,x4
+ff f2 82 93 // addi x5,x5,-1
+fc 02 94 e3 // bne x5,x0,-56
+00 a3 82 33 // add x4,x7,x10
+00 10 00 93 // addi x1,x0,1
 
-    end
-    if (ROB_MemWrite) begin
-        case (ROB_funct3)
-            3'b000: memory[ROB_memadress][7:0] <= out_value[7:0]; // SB
-            3'b001: memory[ROB_memadress][15:0] <= out_value[15:0]; // SH
-            3'b010: memory[ROB_memadress] <= out_value; // SW
-            default: ; // No operation (NOP) for other funct3 values, explicitly defined
-        endcase
-    end
-    // Default value for RData, ensures it is always assigned
-    Data_memory_out = 32'd0; // if MemRead is false
-    if (LS_MemRead) begin
-        case (func3_LS)
-            3'b000: Data_memory_out = {{24{memory[LS_result][31]}}, memory[LS_result][7:0]}; // LB
-            3'b001: Data_memory_out = {{16{memory[LS_result][31]}}, memory[LS_result][15:0]}; // LH
-            3'b010: Data_memory_out = memory[LS_result]; // LW
-            3'b100: Data_memory_out = {{24{1'b0}}, memory[LS_result][7:0]}; // LBU
-            3'b101: Data_memory_out = {{16{1'b0}}, memory[LS_result][15:0]}; // LHU
-            default: Data_memory_out = 32'd0; // Default value assignment to handle cases when MemRead is false
-        endcase
-    end
-  end
+@0000004C
+fe 00 0e e3  // BEQ x0, x0, -4
 
+@000002BC
+00 20 1f 73  // 메모리[700] 명령어
+00 10 1e f3  // 메모리[704] 명령어
+35 c0 0e 67  // 메모리[708] 명령어
 
-   always @(*) begin
-    exception_datamem=0;
-    
-    if (ROB_MemRead ) begin  // 08.07 new update
-    case (ROB_funct3)
-            3'b000: load_data = {{24{memory[ROB_memadress][31]}}, memory[ROB_memadress][7:0]}; // LB
-            3'b001: load_data = {{16{memory[ROB_memadress][31]}}, memory[ROB_memadress][15:0]}; // LH
-            3'b010: load_data = memory[ROB_memadress]; // LW
-            3'b100: load_data = {{24{1'b0}}, memory[ROB_memadress][7:0]}; // LBU
-            3'b101: load_data = {{16{1'b0}}, memory[ROB_memadress][15:0]}; // LHU
-            default: load_data = 32'd0; // Default value assignment to handle cases when MemRead is false
-        endcase
-        if (load_data != out_value) begin
-        exception_datamem = 1;
-        end else begin
-        exception_datamem=0;
-        end
-        end else begin
-        exception_datamem=0;
-        end
-  end
+@000002E4
+00 20 1f 73  // 메모리[740] 명령어
+00 10 1e f3  // 메모리[744] 명령어
+35 c0 0e 67  // 메모리[748] 명령어
 
-endmodulemodule DataMemory(
-    input wire ROB_MemWrite,
-    input wire [31:0] ROB_memadress,
-    input wire [2:0] ROB_funct3,
-    input wire clk,
-    input wire reset,
-    input wire [2:0] func3_LS,
-    input wire [31:0] out_value,
-    input wire [31:0] LS_result,
-    input wire LS_MemRead,
-    
-    input wire ROB_MemRead,  // 08.07 new update
-    output reg exception_datamem,
-    output reg [31:0] Data_memory_out
-);
-reg [31:0]load_data;   //08.07 new update
-integer i;
-reg [31:0] memory [0:4095];
-    always @(posedge clk) begin                       
-   if (reset) begin
-       for (i = 0; i < 2048; i = i + 1) begin
-           memory[i] <= i+3;
-        end
+@0000030C
+30 20 00 73  // 메모리[780] 명령어
 
-    end
-    if (ROB_MemWrite) begin
-        case (ROB_funct3)
-            3'b000: memory[ROB_memadress][7:0] <= out_value[7:0]; // SB
-            3'b001: memory[ROB_memadress][15:0] <= out_value[15:0]; // SH
-            3'b010: memory[ROB_memadress] <= out_value; // SW
-            default: ; // No operation (NOP) for other funct3 values, explicitly defined
-        endcase
-    end
-    // Default value for RData, ensures it is always assigned
-    Data_memory_out = 32'd0; // if MemRead is false
-    if (LS_MemRead) begin
-        case (func3_LS)
-            3'b000: Data_memory_out = {{24{memory[LS_result][31]}}, memory[LS_result][7:0]}; // LB
-            3'b001: Data_memory_out = {{16{memory[LS_result][31]}}, memory[LS_result][15:0]}; // LH
-            3'b010: Data_memory_out = memory[LS_result]; // LW
-            3'b100: Data_memory_out = {{24{1'b0}}, memory[LS_result][7:0]}; // LBU
-            3'b101: Data_memory_out = {{16{1'b0}}, memory[LS_result][15:0]}; // LHU
-            default: Data_memory_out = 32'd0; // Default value assignment to handle cases when MemRead is false
-        endcase
-    end
-  end
+@00000334
+00 20 1f 73  // 메모리[820] 명령어
+00 10 1e f3  // 메모리[824] 명령어
+35 c0 0e 67  // 메모리[828] 명령어
 
-
-   always @(*) begin
-    exception_datamem=0;
-    
-    if (ROB_MemRead ) begin  // 08.07 new update
-    case (ROB_funct3)
-            3'b000: load_data = {{24{memory[ROB_memadress][31]}}, memory[ROB_memadress][7:0]}; // LB
-            3'b001: load_data = {{16{memory[ROB_memadress][31]}}, memory[ROB_memadress][15:0]}; // LH
-            3'b010: load_data = memory[ROB_memadress]; // LW
-            3'b100: load_data = {{24{1'b0}}, memory[ROB_memadress][7:0]}; // LBU
-            3'b101: load_data = {{16{1'b0}}, memory[ROB_memadress][15:0]}; // LHU
-            default: load_data = 32'd0; // Default value assignment to handle cases when MemRead is false
-        endcase
-        if (load_data != out_value) begin
-        exception_datamem = 1;
-        end else begin
-        exception_datamem=0;
-        end
-        end else begin
-        exception_datamem=0;
-        end
-  end
-
-endmodule
+@0000035C
+fe 00 0c e3  // 메모리[860] 명령어
