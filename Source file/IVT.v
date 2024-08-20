ee080bd1bf46427d17bdcee7193633dbffee7225
jjurijang <165987742+jjurijang@users.noreply.github.com>
2024-08-20 14:14:19 +0900

    Update IVT.v

--- Source file/IVT.v
+++ Source file/IVT.v
@@ -5,13 +5,13 @@ module IVT(
 
 always @(*) begin
     if (rob_cause== 2'b00) begin
-        handler_address <= 32'd700; // Illegal Instruction: 700踰덉?
+        handler_address <= 32'd1700; // Illegal Instruction: 700踰덉?
     end else if (rob_cause == 2'b01) begin
-        handler_address <= 32'd740; // DIV 0: 740踰덉?
+        handler_address <= 32'd1740; // DIV 0: 740踰덉?
     end else if (rob_cause == 2'b10) begin
-        handler_address <= 32'd780; // LS: 780踰덉?
+        handler_address <= 32'd1780; // LS: 780踰덉?
     end else if (rob_cause == 2'b11) begin
-        handler_address <= 32'd820; // Address: 820踰덉?
+        handler_address <= 32'd1820; // Address: 820踰덉?
     end
     end
 endmodule
